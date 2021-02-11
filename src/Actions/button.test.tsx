e2ded4955bc27b59c40dedda0bd868bf79fa804a
Jake McBride <pinindajin@gmail.com>
2021-02-10 19:01:09 -0700

    Component Test (need better test??) for Button. Removed ExampleComponent and it's test.

---
+++ src/Actions/button.test.tsx
@@ -0,0 +1,7 @@
+import { Button } from './button'
+
+describe('ExampleComponent', () => {
+  it('is truthy', () => {
+    expect(Button).toBeTruthy()
+  })
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,3 @@
-import * as React from 'react'
 import { Button } from './Actions'
-import styles from './styles.module.css'
 
-interface Props {
-  text: string
-}
-
-const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
-export { Button, ExampleComponent }
+export { Button }
