b05a12f2349b8ba2f59e0e4ae9152f2063905f47
Kupono Yuen <kupono@gmail.com>
2021-02-16 11:43:13 -0700

    Update stories to simpler format

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta } from '@storybook/react/types-6-0';
 
-import { Button, ButtonProps } from '../Actions/button';
+import { Button } from '../Actions/button';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default {
@@ -10,67 +10,24 @@ export default {
   component: Button,
 } as Meta;
 
-const createTemplate = (children: JSX.Element | string) => (args: ButtonProps) => <Button {...args}>{children}</Button>;
+const buttonText = "PizzaTaco"
 
-const TextTemplate = createTemplate("PizzaTaco")
+export const Small = () => <Button size="small">{buttonText}</Button>
+export const Medium = () => <Button size="medium">{buttonText}</Button>
+export const Large = () => <Button size="large">{buttonText}</Button>
 
-export const Small = () => <Button size="small">PizzaTaco</Button>
+export const OutlinedSmall = () => <Button size="small" outlinedStyle={true}>{buttonText}</Button>
+export const OutlinedMedium = () => <Button size="medium" outlinedStyle={true}>{buttonText}</Button>
+export const OutlinedLarge = () => <Button size="large" outlinedStyle={true}>{buttonText}</Button>
 
-export const Medium = TextTemplate.bind({});
-Medium.args = {
-  size: "medium"
-} as ButtonProps;
+export const DisabledSmall = () => <Button size="small" disabled={true}>{buttonText}</Button>
+export const DisabledMedium = () => <Button size="medium" disabled={true}>{buttonText}</Button>
+export const DisabledLarge = () => <Button size="large" disabled={true}>{buttonText}</Button>
 
-export const Large = TextTemplate.bind({});
-Large.args = {
-  size: 'large'
-} as ButtonProps;
+export const DisabledOutlinedSmall = () => <Button size="small" outlinedStyle={true} disabled={true}>{buttonText}</Button>
+export const DisabledOutlinedMedium = () => <Button size="medium" outlinedStyle={true} disabled={true}>{buttonText}</Button>
+export const DisabledOutlinedLarge = () => <Button size="large" outlinedStyle={true} disabled={true}>{buttonText}</Button>
 
-export const OutlinedSmall = TextTemplate.bind({});
-OutlinedSmall.args = {
-  size: 'small',
-  outlinedStyle: true,
-} as ButtonProps;
-
-export const OutlinedMedium = TextTemplate.bind({});
-OutlinedMedium.args = {
-  size: "medium",
-  outlinedStyle: true,
-} as ButtonProps;
-
-export const OutlinedLarge = TextTemplate.bind({});
-OutlinedLarge.args = {
-  size: 'large',
-  outlinedStyle: true,
-} as ButtonProps;
-
-export const DisabledMedium = TextTemplate.bind({});
-DisabledMedium.args = {
-  size: 'medium',
-  disabled: true
-} as ButtonProps;
-
-export const DisabledOutlinedMedium = TextTemplate.bind({});
-DisabledOutlinedMedium.args = {
-  size: "medium",
-  outlinedStyle: true,
-  disabled: true,
-} as ButtonProps;
-
-export const SmallIcon = TextTemplate.bind({});
-SmallIcon.args = {
-  icon: <ExpandMoreIcon />,
-  size: 'small'
-} as ButtonProps;
-
-export const MediumIcon = TextTemplate.bind({});
-MediumIcon.args = {
-  icon: <ExpandMoreIcon />,
-  size: 'medium'
-} as ButtonProps;
-
-export const LargeIcon = TextTemplate.bind({});
-LargeIcon.args = {
-  icon: <ExpandMoreIcon />,
-  size: 'large'
-} as ButtonProps;
+export const SmallIcon = () => <Button size="small" icon={<ExpandMoreIcon />}></Button>
+export const MediumIcon = () => <Button size="medium" icon={<ExpandMoreIcon />}></Button>
+export const LargeIcon = () => <Button size="large" icon={<ExpandMoreIcon />}></Button>
