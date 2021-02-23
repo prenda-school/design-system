0ad8df077596ebb9f0c6929030ab2f4f6b3181b5
smrbts <samueljustinroberts@gmail.com>
2021-02-23 15:27:23 -0600

    small input field now has focus and blur effects

--- src/form-inputs/input.tsx
+++ src/form-inputs/input.tsx
@@ -6,21 +6,23 @@ import {
 } from '@material-ui/core/';
 import { withStyles } from '@material-ui/core/styles';
 
+//TODO: apparently to alter the Input field, you need to pass InputProps as a prop? Don't like the look of this,
+// open to suggestions on changes.
 export type InputProps = {
   inputId: string;
   label?: string;
   bottomInputLabel?: string;
   inputPlaceholder?: string;
+  InputProps?: string;
 };
 
 export const Input: FC<InputProps> = props => {
-
   const InputLabel = withStyles({
     root: {
       color: '#072E44',
       top: '-5px',
       'font-weight': 700,
-      'font-size': '10px',
+      'font-size': '12px',
       'line-height': '18px',
       '&.Mui-focused': {
         color: '#072E44',
@@ -33,27 +35,35 @@ export const Input: FC<InputProps> = props => {
       border: '1px solid #D2D4D6',
       'box-sizing': 'border-box',
       'border-radius': '8px',
-      'padding-left': '16px',
-      
+      '& .MuiInputBase-input': {
+        'padding-left': '16px',
+      },
+      '& .Mui-focused': {
+        border: '1px solid #498DCC',
+        'box-sizing': 'border-box',
+        'box-shadow': '0px 0px 0px 4px #D7F3FF',
+        'border-radius': '8px',
+      },
     },
   })(MatTextField);
 
   const InputHelperText = withStyles({
     root: {
       color: 'rgba(7, 46, 68, 0.72)',
-      'font-size': '8px',
+      'font-size': '10px',
       'line-height': '18px',
     },
   })(MatFormHelperText);
 
-
   return (
-    <div>
+    <>
       <InputLabel>{props.label}</InputLabel>
       <InputTextField
+        id={props.inputId}
         defaultValue={props.inputPlaceholder}
+        InputProps={{ disableUnderline: true }}
       ></InputTextField>
       <InputHelperText>{props.bottomInputLabel}</InputHelperText>
-    </div>
+    </>
   );
 };
