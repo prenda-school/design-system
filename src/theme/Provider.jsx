d9a2b2c5d61209cb1cae10fe5cf1449cd55972b6
William Kelley <williamkelley9@gmail.com>
2021-03-10 17:09:48 -0500

    Chg: add StylesProvider HOC

--- src/theme/Provider.jsx
+++ src/theme/Provider.jsx
@@ -1,21 +1,19 @@
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { muiTheme } from './theme';
 
 export const SparkThemeProvider = function ({ children }) {
   return (
-    <ThemeProvider theme={muiTheme}>
-      <StyledThemeProvider theme={muiTheme}>{children}</StyledThemeProvider>
-    </ThemeProvider>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={muiTheme}>
+        <StyledThemeProvider theme={muiTheme}>{children}</StyledThemeProvider>
+      </ThemeProvider>
+    </StylesProvider>
   );
 };
 
-// Consumer
+// Consumer Example
 
-{
-  /* <SparkThemeProvider>
-  <App />
-</SparkThemeProvider> */
-}
-
-// useTheme();
+//  <SparkThemeProvider>
+//    <App />
+//  </SparkThemeProvider>
