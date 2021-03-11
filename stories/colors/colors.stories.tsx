c901d357daf6543cc05c7d3b601e3c17d0e0e2ac
Jake McBride <pinindajin@gmail.com>
2021-03-10 22:18:01 -0700

    Updated story to show all colors

--- stories/colors/colors.stories.tsx
+++ stories/colors/colors.stories.tsx
@@ -5,9 +5,9 @@ import { colors } from '../../src';
 import styled from 'styled-components';
 
 interface ColorBoxProps {
+  color: string;
+  colorLabel: string;
   textColor: string;
-  colorToTest: string;
-  label: string;
 }
 
 const ColorBox: FC<ColorBoxProps> = props => {
@@ -15,20 +15,21 @@ const ColorBox: FC<ColorBoxProps> = props => {
     <div
       style={{
         color: props.textColor,
-        backgroundColor: props.colorToTest,
-        width: '135px',
-        height: '135px',
+        backgroundColor: props.color,
+        width: '13em',
+        height: '13em',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'flex-start',
         flexDirection: 'row',
         paddingTop: '5px',
-        fontSize: '14px',
+        fontSize: '0.80em',
+        margin: '1px',
       }}
     >
       <div>
-        <div>{props.label}</div>
-        <div>{props.colorToTest}</div>
+        <div>{props.colorLabel}</div>
+        <div>{props.color}</div>
       </div>
     </div>
   );
@@ -41,27 +42,142 @@ export default {
 
 export const PrendaColors = () => (
   <TextFormatter>
+    <ColorContainerTitle>Prenda colors</ColorContainerTitle>
+    <ColorContainer>
+      <RowContainer>
+        <ColorBox 
+          color={colors.colorsPrendaBlue}
+          textColor={colors.colorsTextIconOnDarkHighContrast}
+          colorLabel="colorsPrendaBlue"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaLightBlue}
+          textColor={colors.colorsTextIconOnLightHighContrast}
+          colorLabel="colorsPrendaLightBlue"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaOrange}
+          textColor={colors.colorsTextIconOnDarkHighContrast}
+          colorLabel="colorsPrendaOrange"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaPeach}
+          textColor={colors.colorsTextIconOnLightHighContrast}
+          colorLabel="colorsPrendaPeach"
+        />
+      </RowContainer>
+    </ColorContainer>
+    <ColorContainer>
+      <RowContainer>
+        <ColorBox 
+          color={colors.colorsPrendaNavy}
+          textColor={colors.colorsTextIconOnDarkHighContrast}
+          colorLabel="colorsPrendaNavy"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaLightGrey}
+          textColor={colors.colorsTextIconOnLightHighContrast}
+          colorLabel="colorsPrendaLightGrey"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaMediumGrey}
+          textColor={colors.colorsTextIconOnLightHighContrast}
+          colorLabel="colorsPrendaMediumGrey"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaDarkGrey}
+          textColor={colors.colorsTextIconOnLightHighContrast}
+          colorLabel="colorsPrendaDarkGrey"
+        />
+        <ColorBox 
+          color={colors.colorsPrendaWhite}
+          textColor={colors.colorsTextIconOnLightHighContrast}
+          colorLabel="colorsPrendaWhite"
+        />
+      </RowContainer>
+    </ColorContainer>
+
+    <ColorContainerTitle>Background colors</ColorContainerTitle>
+    <ColorContainer>
+      <RowContainer>
+        <ColorBox 
+          color={colors.colorsBackgroundNavy} 
+          textColor={colors.colorsTextIconOnDarkHighContrast} 
+          colorLabel="colorsBackgroundNavy"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundNavy} 
+          textColor={colors.colorsTextIconOnDarkLowContrast} 
+          colorLabel="colorsBackgroundNavy"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundBlue} 
+          textColor={colors.colorsTextIconOnDarkHighContrast} 
+          colorLabel="colorsBackgroundBlue"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundBlue} 
+          textColor={colors.colorsTextIconOnDarkLowContrast} 
+          colorLabel="colorsBackgroundBlue"
+        />
+      </RowContainer>
+    </ColorContainer>
+    <ColorContainer>
+      <RowContainer>
+        <ColorBox 
+          color={colors.colorsBackgroundLightGrey} 
+          textColor={colors.colorsTextIconOnLightHighContrast} 
+          colorLabel="colorsBackgroundNavy"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundLightGrey} 
+          textColor={colors.colorsTextIconOnLightLowContrast} 
+          colorLabel="colorsBackgroundNavy"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundLightBlue} 
+          textColor={colors.colorsTextIconOnLightHighContrast} 
+          colorLabel="colorsBackgroundBlue"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundLightBlue} 
+          textColor={colors.colorsTextIconOnLightLowContrast} 
+          colorLabel="colorsBackgroundBlue"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundWhite} 
+          textColor={colors.colorsTextIconOnLightHighContrast} 
+          colorLabel="colorsBackgroundBlue"
+        />
+        <ColorBox 
+          color={colors.colorsBackgroundWhite} 
+          textColor={colors.colorsTextIconOnLightLowContrast} 
+          colorLabel="colorsBackgroundBlue"
+        />
+      </RowContainer>
+    </ColorContainer>
+
     <ColorContainerTitle>Product colors</ColorContainerTitle>
     <ColorContainer>
       <ColorBox
-        colorToTest={colors.colorsPrimaryAction}
+        color={colors.colorsPrimaryAction}
         textColor={colors.colorsTextIconOnDarkHighContrast}
-        label="colorsPrimaryAction"
+        colorLabel="colorsPrimaryAction"
       />
       <ColorBox
-        colorToTest={colors.colorsSuccess}
+        color={colors.colorsSuccess}
         textColor={colors.colorsTextIconOnDarkHighContrast}
-        label="colorsSuccess"
+        colorLabel="colorsSuccess"
       />
       <ColorBox
-        colorToTest={colors.colorsWarning}
+        color={colors.colorsWarning}
         textColor={colors.colorsTextIconOnLightHighContrast}
-        label="colorsWarning"
+        colorLabel="colorsWarning"
       />
       <ColorBox
-        colorToTest={colors.colorsError}
+        color={colors.colorsError}
         textColor={colors.colorsTextIconOnDarkHighContrast}
-        label="colorsError"
+        colorLabel="colorsError"
       />
     </ColorContainer>
 
@@ -69,170 +185,174 @@ export const PrendaColors = () => (
     <ColorContainer>
       <div>
         <ColorBox
-          colorToTest={colors.colorsRed[1]}
+          color={colors.colorsRed[1]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsRed[1]"
+          colorLabel="colorsRed[1]"
         />
         <ColorBox
-          colorToTest={colors.colorsRed[2]}
+          color={colors.colorsRed[2]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsRed[2]"
+          colorLabel="colorsRed[2]"
         />
         <ColorBox
-          colorToTest={colors.colorsRed[3]}
+          color={colors.colorsRed[3]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsRed[3]"
+          colorLabel="colorsRed[3]"
         />
         <ColorBox
-          colorToTest={colors.colorsRed[4]}
+          color={colors.colorsRed[4]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsRed[4]"
+          colorLabel="colorsRed[4]"
         />
         <ColorBox
-          colorToTest={colors.colorsRed[5]}
+          color={colors.colorsRed[5]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsRed[5]"
+          colorLabel="colorsRed[5]"
         />
       </div>
       <div>
         <ColorBox
-          colorToTest={colors.colorsOrange[1]}
+          color={colors.colorsOrange[1]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsOrange[1]"
+          colorLabel="colorsOrange[1]"
         />
         <ColorBox
-          colorToTest={colors.colorsOrange[2]}
+          color={colors.colorsOrange[2]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsOrange[2]"
+          colorLabel="colorsOrange[2]"
         />
         <ColorBox
-          colorToTest={colors.colorsOrange[3]}
+          color={colors.colorsOrange[3]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsOrange[3]"
+          colorLabel="colorsOrange[3]"
         />
         <ColorBox
-          colorToTest={colors.colorsOrange[4]}
+          color={colors.colorsOrange[4]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsOrange[4]"
+          colorLabel="colorsOrange[4]"
         />
         <ColorBox
-          colorToTest={colors.colorsOrange[5]}
+          color={colors.colorsOrange[5]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsOrange[5]"
+          colorLabel="colorsOrange[5]"
         />
       </div>
       <div>
         <ColorBox
-          colorToTest={colors.colorsYellow[1]}
+          color={colors.colorsYellow[1]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsYellow[1]"
+          colorLabel="colorsYellow[1]"
         />
         <ColorBox
-          colorToTest={colors.colorsYellow[2]}
+          color={colors.colorsYellow[2]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsYellow[2]"
+          colorLabel="colorsYellow[2]"
         />
         <ColorBox
-          colorToTest={colors.colorsYellow[3]}
+          color={colors.colorsYellow[3]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsYellow[3]"
+          colorLabel="colorsYellow[3]"
         />
         <ColorBox
-          colorToTest={colors.colorsYellow[4]}
+          color={colors.colorsYellow[4]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsYellow[4]"
+          colorLabel="colorsYellow[4]"
         />
         <ColorBox
-          colorToTest={colors.colorsYellow[5]}
+          color={colors.colorsYellow[5]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsYellow[5]"
+          colorLabel="colorsYellow[5]"
         />
       </div>
       <div>
         <ColorBox
-          colorToTest={colors.colorsGreen[1]}
+          color={colors.colorsGreen[1]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsGreen[1]"
+          colorLabel="colorsGreen[1]"
         />
         <ColorBox
-          colorToTest={colors.colorsGreen[2]}
+          color={colors.colorsGreen[2]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsGreen[2]"
+          colorLabel="colorsGreen[2]"
         />
         <ColorBox
-          colorToTest={colors.colorsGreen[3]}
+          color={colors.colorsGreen[3]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsGreen[3]"
+          colorLabel="colorsGreen[3]"
         />
         <ColorBox
-          colorToTest={colors.colorsGreen[4]}
+          color={colors.colorsGreen[4]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsGreen[4]"
+          colorLabel="colorsGreen[4]"
         />
         <ColorBox
-          colorToTest={colors.colorsGreen[5]}
+          color={colors.colorsGreen[5]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsGreen[5]"
+          colorLabel="colorsGreen[5]"
         />
       </div>
       <div>
         <ColorBox
-          colorToTest={colors.colorsBlue[1]}
+          color={colors.colorsBlue[1]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsBlue[1]"
+          colorLabel="colorsBlue[1]"
         />
         <ColorBox
-          colorToTest={colors.colorsBlue[2]}
+          color={colors.colorsBlue[2]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsBlue[2]"
+          colorLabel="colorsBlue[2]"
         />
         <ColorBox
-          colorToTest={colors.colorsBlue[3]}
+          color={colors.colorsBlue[3]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsBlue[3]"
+          colorLabel="colorsBlue[3]"
         />
         <ColorBox
-          colorToTest={colors.colorsBlue[4]}
+          color={colors.colorsBlue[4]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsBlue[4]"
+          colorLabel="colorsBlue[4]"
         />
         <ColorBox
-          colorToTest={colors.colorsBlue[5]}
+          color={colors.colorsBlue[5]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsBlue[5]"
+          colorLabel="colorsBlue[5]"
         />
       </div>
       <div>
         <ColorBox
-          colorToTest={colors.colorsPurple[1]}
+          color={colors.colorsPurple[1]}
           textColor={colors.colorsTextIconOnLightHighContrast}
-          label="colorsPurple[1]"
+          colorLabel="colorsPurple[1]"
         />
         <ColorBox
-          colorToTest={colors.colorsPurple[2]}
+          color={colors.colorsPurple[2]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsPurple[2]"
+          colorLabel="colorsPurple[2]"
         />
         <ColorBox
-          colorToTest={colors.colorsPurple[3]}
+          color={colors.colorsPurple[3]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsPurple[3]"
+          colorLabel="colorsPurple[3]"
         />
         <ColorBox
-          colorToTest={colors.colorsPurple[4]}
+          color={colors.colorsPurple[4]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsPurple[4]"
+          colorLabel="colorsPurple[4]"
         />
         <ColorBox
-          colorToTest={colors.colorsPurple[5]}
+          color={colors.colorsPurple[5]}
           textColor={colors.colorsTextIconOnDarkHighContrast}
-          label="colorsPurple[5]"
+          colorLabel="colorsPurple[5]"
         />
       </div>
     </ColorContainer>
   </TextFormatter>
 );
 
+const RowContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 const ColorContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -242,6 +362,7 @@ const TextFormatter = styled.div`
   font-family: 'Nunito Sans', -apple-system, '.SFNSText-Regular',
     'San Francisco', BlinkMacSystemFont, 'Segoe UI', 'Helvetica Neue', Helvetica,
     Arial, sans-serif;
+  background-color: #eaeaea;
 `;
 const ColorContainerTitle = styled.div`
   font-weight: 500;
