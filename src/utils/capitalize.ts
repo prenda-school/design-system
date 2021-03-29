55a65ec80fc75127a294022cb2804c53a158c0ee
William Kelley <williamkelley9@gmail.com>
2021-03-29 17:37:14 -0400

    Chore: lint

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,7 +1,9 @@
 /** Uppercase the first letter. */
 export default function capitalize(string: string) {
   if (typeof string !== 'string') {
-    throw new Error('Prenda Spark: capitalize(string) expects a string argument');
+    throw new Error(
+      'Prenda Spark: capitalize(string) expects a string argument'
+    );
   }
 
   return string.charAt(0).toUpperCase() + string.slice(1);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,1 @@
-export { default as capitalize } from "./capitalize";
+export { default as capitalize } from './capitalize';
