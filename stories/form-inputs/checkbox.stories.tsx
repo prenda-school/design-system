2cf9aa5a2a28ed63adb3909273340a7a3c4f9999
William Kelley <williamkelley9@gmail.com>
2021-04-30 00:26:56 -0400

    Refactor: checkbox with true-er icon's, correct sizing, styled-comp, props pass through

--- stories/form-inputs/checkbox.stories.tsx
+++ stories/form-inputs/checkbox.stories.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story } from '@storybook/react';
 import { Meta } from '@storybook/react/types-6-0';
-
-import { Checkbox } from '../../dist';
+import { Checkbox, CheckboxProps } from '../../src';
 
 export default {
   title: 'prenda-spark/Checkbox',
   component: Checkbox,
+  argTypes: {
+    label: { control: 'text' },
+    checked: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+  },
+  args: {
+    label: 'Checkbox label',
+  },
 } as Meta;
 
-export const BasicCheckbox = () => (
-  <Checkbox label="Basic checkbox" value="basic" name="basicCheckbox" />
-);
-export const CheckedCheckbox = () => (
-  <Checkbox
-    label="Checked checkbox"
-    value="checked"
-    name="checkedCheckbox"
-    checked={true}
-  />
-);
-export const DisabledCheckbox = () => (
-  <Checkbox
-    label="Disabled checkbox"
-    value="disabled"
-    name="disabledCheckbox"
-    disabled={true}
-  />
-);
+const Template: Story<CheckboxProps> = args => <Checkbox {...args} />;
+export const ConfigurableInput = Template.bind({});
