90477858712a3244d4f4fcdeeeaf45e1d26da33f
William Kelley <williamkelley9@gmail.com>
2021-05-03 14:33:49 -0400

    Del: unused import

--- stories/form-inputs/input.stories.tsx
+++ stories/form-inputs/input.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story } from '@storybook/react';
 import { Meta } from '@storybook/react/types-6-0';
-import { StoryTitle, TextFormatter, StorySubTitle } from '../story-util';
 
 import { Input, InputProps } from '../../src';
 
