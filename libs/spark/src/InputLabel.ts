3cca0f58cc1a0fe44e2cf855ea3f0ad66bae3c00
William Kelley <williamkelley9@gmail.com>
2021-06-28 18:37:46 -0400

    fix root classes key not defined

--- libs/spark/src/InputLabel.ts
+++ libs/spark/src/InputLabel.ts
@@ -1,13 +1,15 @@
 import { colors } from './theme/colors';
 
 export const MuiInputLabelStyleOverrides = {
-  color: colors.colorsTextIconOnLightHighContrast,
-  marginBottom: '0.5rem', // 8px
-  fontWeight: 700,
-  fontSize: '1rem', // 16px
-  lineHeight: '1.125rem', // 18px
+  root: {
+    color: colors.colorsTextIconOnLightHighContrast,
+    marginBottom: '0.5rem', // 8px
+    fontWeight: 700,
+    fontSize: '1rem', // 16px
+    lineHeight: '1.125rem', // 18px
 
-  disabled: {
-    color: colors.colorsPrendaDarkGrey,
+    disabled: {
+      color: colors.colorsPrendaDarkGrey,
+    },
   },
 };
