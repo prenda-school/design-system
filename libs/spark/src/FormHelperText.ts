e66261e565ffaa8220fc410de3cf6f6c715e143b
William Kelley <williamkelley9@gmail.com>
2021-06-28 19:18:12 -0400

    adds missing disabled helper text styling

--- libs/spark/src/FormHelperText.ts
+++ libs/spark/src/FormHelperText.ts
@@ -6,5 +6,8 @@ export const MuiFormHelperTextStyleOverrides = {
     fontSize: '1rem', // 12px
     lineHeight: '1.5rem', // 20px
     marginTop: '0.5rem', // 8px
+    '&$disabled': {
+      color: colors.colorsPrendaDarkGrey,
+    },
   },
 };
