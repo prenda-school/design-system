892aca5cbc3a0dd8ead5062f8cc20de8cbc6aa43
William Kelley <williamkelley9@gmail.com>
2021-06-29 16:10:48 -0400

    feat(InputLabel): add global styles (#92)

--- libs/spark/src/Input.tsx
+++ libs/spark/src/Input.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import clsx from 'clsx';
 import {
   Input,
-  InputLabel as MuiInputLabel,
+  InputLabel,
   FormHelperText,
   InputProps as MuiInputProps,
   InputLabelProps as MuiInputLabelProps,
@@ -19,20 +19,6 @@ export interface InputProps extends MuiInputProps {
   helperText?: string;
   FormHelperTextProps?: MuiFormHelperTextProps;
 }
-
-const InputLabel = styled(MuiInputLabel)`
-  ${({ theme }: { theme: Theme }) => `
-    color: ${theme.palette.background.lightContrastText};
-    margin-bottom: 0.5rem; // 8px
-    font-weight: 700;
-    font-size: 1rem; // 16px
-    line-height: 1.125rem; // 18px
-
-    &.Mui-disabled {
-      color: ${theme.palette.neutral.darkGrey};
-    }
-  `}
-`;
 
 export const MuiInputPropOverrides = {
   disableUnderline: true,
---
+++ libs/spark/src/InputLabel.ts
@@ -0,0 +1,14 @@
+import { colors } from './theme/colors';
+
+export const MuiInputLabelStyleOverrides = {
+  root: {
+    color: colors.colorsTextIconOnLightHighContrast,
+    marginBottom: '0.5rem', // 8px
+    fontWeight: 700,
+    fontSize: '1rem', // 16px
+    lineHeight: '1.125rem', // 18px
+    '&$disabled': {
+      color: colors.colorsPrendaDarkGrey,
+    },
+  },
+};
