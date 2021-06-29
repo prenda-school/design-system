8ca3b98eea5f784ea2b7497df331e91e54667b7a
William Kelley <williamkelley9@gmail.com>
2021-06-29 16:32:27 -0400

    fix padding being off

--- libs/spark/src/ButtonBase.ts
+++ libs/spark/src/ButtonBase.ts
@@ -17,10 +17,10 @@ export const MuiButtonBaseStyleOverrides = {
         { duration: transitions.duration.short }
       ),
       borderRadius: 8,
-      borderWidth: 2,
+      borderWidth: '2px',
       borderStyle: 'solid' as const,
       fontSize: '1rem', // 16px
-      lineHeight: '1.125rem', // 18px
+      lineHeight: '1.25rem', // 20px
       fontWeight: 700,
       '&.Mui-focusVisible': {
         boxShadow: `0 0 0 4px ${colors.colorsBlue[1]}`,
@@ -28,81 +28,82 @@ export const MuiButtonBaseStyleOverrides = {
       '&$disabled': {
         opacity: '50%',
       },
-    },
-    '&.SparkButton-variantSolid': {
-      borderColor: colors.colorsBlue[3],
-      backgroundColor: colors.colorsBlue[3],
-      color: colors.colorsPrendaWhite,
-      '&:hover': {
-        borderColor: colors.colorsBlue[4],
-        backgroundColor: colors.colorsBlue[4],
-      },
-    },
-    '&.SparkButton-variantOutlined': {
-      borderColor: colors.colorsPrendaMediumGrey,
-      backgroundColor: colors.colorsPrendaWhite,
-      color: colors.colorsBlue[3],
-      '&:hover': {
-        backgroundColor: colors.colorsPrendaLightGrey,
-      },
-    },
-    '&.SparkButton-variantFlat': {
-      borderColor: 'transparent',
-      backgroundColor: 'transparent',
-      color: colors.colorsBlue[3],
-      '&:hover': {
-        color: colors.colorsBlue[4],
-      },
-      '&.Mui-focusVisible': {
+
+      '&.SparkButton-variantSolid': {
         borderColor: colors.colorsBlue[3],
-        backgroundColor: colors.colorsBlue[1],
+        backgroundColor: colors.colorsBlue[3],
+        color: colors.colorsPrendaWhite,
+        '&:hover': {
+          borderColor: colors.colorsBlue[4],
+          backgroundColor: colors.colorsBlue[4],
+        },
       },
-    },
-    // Y-paddings are subtracted by 2 to account for border-width
-    '&.SparkButton-sizeLarge': {
-      fontSize: '1.125rem', // 18px
-      padding: '.675rem 1rem', // 12-2px 16px
-      '.SparkButton-content': {
-        height: '1.5rem', // 24px
+      '&.SparkButton-variantOutlined': {
+        borderColor: colors.colorsPrendaMediumGrey,
+        backgroundColor: colors.colorsPrendaWhite,
+        color: colors.colorsBlue[3],
+        '&:hover': {
+          backgroundColor: colors.colorsPrendaLightGrey,
+        },
       },
-    },
-    '&.SparkButton-sizeMedium': {
-      padding: '.25rem 1rem', // 6-2px 16px
-      '.SparkButton-content': {
-        height: '1.25rem', // 20px
+      '&.SparkButton-variantFlat': {
+        borderColor: 'transparent',
+        backgroundColor: 'transparent',
+        color: colors.colorsBlue[3],
+        '&:hover': {
+          color: colors.colorsBlue[4],
+        },
+        '&.Mui-focusVisible': {
+          borderColor: colors.colorsBlue[3],
+          backgroundColor: colors.colorsBlue[1],
+        },
       },
-    },
-    '&.SparkButton-sizeSmall': {
-      fontSize: '0.75rem', // 12px
-      lineHeight: '0.75rem', // 12px
-      padding: '.125rem 1rem', // 4-2px 16px
+      // Y-paddings are subtracted by 2 to account for border-width
+      '&.SparkButton-sizeLarge': {
+        fontSize: '1.125rem', // 18px
+        padding: '.75rem 1rem', // 12px (accounting for 2px border width) 16px
+        '.SparkButton-content': {
+          height: '1.5rem', // 24px
+        },
+      },
+      '&.SparkButton-sizeMedium': {
+        padding: '.375rem 1rem', // 6px (accounting for 2px border width) 16px
+        '.SparkButton-content': {
+          height: '1.25rem', // 20px
+        },
+      },
+      '&.SparkButton-sizeSmall': {
+        fontSize: '0.75rem', // 12px
+        lineHeight: '0.75rem', // 12px
+        padding: '.25rem 1rem', // 4px (accounting for 2px border width) 16px
+        '& .SparkButton-content': {
+          height: '1rem', // 16px
+        },
+      },
       '& .SparkButton-content': {
-        height: '1rem', // 16px
+        // taken from Mui
+        width: '100%', // Ensure the correct width for iOS Safari
+        display: 'inherit',
+        alignItems: 'inherit',
+        justifyContent: 'inherit',
+        '& .SparkButton-startIcon': {
+          display: 'inherit',
+          marginRight: '.5rem', // 8px
+        },
+        '& .SparkButton-endIcon': {
+          display: 'inherit',
+          marginLeft: '.5rem', // 8px
+        },
+        '& .SparkButton-iconSizeSmall': {
+          fontSize: '1rem', // 16px
+        },
+        '& .SparkButton-iconSizeMedium': {
+          fontSize: '1.25rem', // 20px
+        },
+        '& .SparkButton-iconSizeLarge': {
+          fontSize: '1.5rem', // 24px
+        },
       },
-    },
-    '.SparkButton-content': {
-      // taken from Mui
-      width: '100%', // Ensure the correct width for iOS Safari
-      display: 'inherit',
-      alignItems: 'inherit',
-      justifyContent: 'inherit',
-    },
-    '.SparkButton-startIcon': {
-      display: 'inherit',
-      marginRight: '.5rem', // 8px
-    },
-    '.SparkButton-endIcon': {
-      display: 'inherit',
-      marginLeft: '.5rem', // 8px
-    },
-    '.SparkButton-iconSizeSmall': {
-      fontSize: '1rem', // 16px
-    },
-    '.SparkButton-iconSizeMedium': {
-      fontSize: '1.25rem', // 20px
-    },
-    '.SparkButton-iconSizeLarge': {
-      fontSize: '1.5rem', // 24px
     },
   },
 };
