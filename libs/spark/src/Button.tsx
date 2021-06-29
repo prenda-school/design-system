6761097329d36ca564d9ab2b34f15123811f4ec6
William Kelley <williamkelley9@gmail.com>
2021-06-29 16:58:37 -0400

    feat(ButtonBase): add global styles and props (#98)
    * feat(ButtonBase): add global styles and props
    
    * fix bad style names
    
    * scope ButtonBase style overrides to SparkButton
    
    * fix padding being off
    
    * fix vertical stylings

--- libs/spark/src/Button.tsx
+++ libs/spark/src/Button.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import {
-  ButtonBase as MuiButtonBase,
+  ButtonBase,
   ButtonBaseProps as MuiButtonBaseProps,
   Theme,
 } from '@material-ui/core';
@@ -8,130 +8,12 @@ import styled from 'styled-components';
 import clsx from 'clsx';
 import { capitalize } from './utils';
 
-export interface ButtonProps
-  extends Omit<
-    MuiButtonBaseProps,
-    | 'disableRipple'
-    | 'disableTouchRipple'
-    | 'focusRipple'
-    | 'centerRipple'
-    | 'TouchRippleProps'
-  > {
+export interface ButtonProps extends MuiButtonBaseProps {
   variant?: 'solid' | 'outlined' | 'flat';
   size?: 'large' | 'medium' | 'small';
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
 }
-
-// TODO: file Figma issue: all borders should be "outside" strokes; otherwise they should be box-shadows; the main issue produced is spacing discrepancies (e.g. 12px of padding, but a center stroke of 2px width cuts the padding by 1px inside to 11px in effect). NOTE: this does not apply to icons
-
-export const ButtonBase = styled(MuiButtonBase)`
-  ${({ theme }: { theme: Theme }) => `
-    font-family: ${theme.typography.button.fontFamily};
-
-    font-size: 1rem; // 16px;
-    line-height: 1.125rem; // 18px;
-    font-weight: 700;
-    border-radius: 8px;
-    border-width: 2px;
-    border-style: solid;
-
-      // FROM-MUI
-    box-sizing: border-box;
-    transition: ${theme.transitions.create(
-      ['background-color', 'box-shadow', 'border'],
-      {
-        duration: theme.transitions.duration.short,
-      }
-    )};
-    
-    &.Mui-focusVisible {
-      box-shadow: 0 0 0 4px ${theme.palette.tertiary.blue[1]};
-    }
-    &.Mui-disabled {
-      opacity: 50%;
-    }
-
-        &.SparkButton-variantSolid {
-      border-color: ${theme.palette.tertiary.blue[3]};
-      background-color: ${theme.palette.tertiary.blue[3]};
-      color: white;
-      &:hover {
-        border-color: ${theme.palette.tertiary.blue[4]};
-        background-color: ${theme.palette.tertiary.blue[4]};
-      }
-    }
-    &.SparkButton-variantOutlined {
-      border-color: ${theme.palette.neutral.mediumGrey};
-      background-color: white;
-      color: ${theme.palette.tertiary.blue[3]};
-      &:hover {
-        background-color: ${theme.palette.neutral.lightGrey};
-      }
-    }
-    &.SparkButton-variantFlat {
-      border-color: transparent;
-      background-color: transparent;
-      color: ${theme.palette.tertiary.blue[3]};
-      &:hover {
-        color: ${theme.palette.tertiary.blue[4]};
-      }
-      &.Mui-focusVisible {
-        border-color: ${theme.palette.tertiary.blue[3]};
-        background-color: ${theme.palette.tertiary.blue[1]};
-      }
-    }
-    // Y-paddings are subtracted by 2 to account for border-width
-    &.SparkButton-sizeLarge {
-      font-size: 1.125rem; // 18px
-      padding: .675rem 1rem; // 12-2px 16px
-      & .SparkButton-content {
-        height: 1.5rem; // 24px
-      }
-    }
-    &.SparkButton-sizeMedium {
-      padding: .25rem 1rem; // 6-2px 16px
-      & .SparkButton-content {
-        height: 1.25rem; // 20px
-      }
-    }
-    &.SparkButton-sizeSmall {
-      font-size: 0.75rem; // 12px
-      line-height: 0.75rem; // 12px
-      padding: .125rem 1rem; // 4-2px 16px
-      & .SparkButton-content {
-        height: 1rem; // 16px
-      }
-    }
-
-        & .SparkButton-content {
-      // FROM-MUI
-      width: 100%; // Ensure the correct width for iOS Safari
-      display: inherit;
-      align-items: inherit;
-      justify-content: inherit;
-    }
-
-        & .SparkButton-startIcon {
-      display: inherit;
-      margin-right: .5rem; // 8px
-    }
-    & .SparkButton-endIcon {
-      display: inherit;
-      margin-left: .5rem; // 8px
-    }
-
-        & .SparkButton-iconSizeSmall {
-      font-size: 1rem; // 16px
-    }
-    & .SparkButton-iconSizeMedium {
-      font-size: 1.25rem; // 20px
-    }
-    & .SparkButton-iconSizeLarge {
-      font-size: 1.5rem; // 24px
-    }
-  `}
-`;
 
 const SparkButton: FC<ButtonProps> = ({
   className,
@@ -144,9 +26,6 @@ const SparkButton: FC<ButtonProps> = ({
 }) => {
   return (
     <ButtonBase
-      disableRipple={true}
-      disableTouchRipple={true}
-      focusRipple={false}
       className={clsx(
         className,
         'SparkButton-root',
---
+++ libs/spark/src/ButtonBase.ts
@@ -0,0 +1,101 @@
+import transitions from '@material-ui/core/styles/transitions';
+import { colors } from './theme/colors';
+
+export const MuiButtonBasePropOverrides = {
+  disableRipple: true,
+  disableTouchRipple: true,
+  focusRipple: false,
+};
+
+export const MuiButtonBaseStyleOverrides = {
+  root: {
+    '&.SparkButton-root': {
+      fontFamily: '"Nunito", Avenir, sans-serif',
+      boxSizing: 'border-box' as const,
+      transition: transitions.create(
+        ['background-color', 'box-shadow', 'border'],
+        { duration: transitions.duration.short }
+      ),
+      borderRadius: 8,
+      borderWidth: '2px',
+      borderStyle: 'solid' as const,
+      fontWeight: 700,
+      '&.Mui-focusVisible': {
+        boxShadow: `0 0 0 4px ${colors.colorsBlue[1]}`,
+      },
+      '&$disabled': {
+        opacity: '50%',
+      },
+
+      '&.SparkButton-variantSolid': {
+        borderColor: colors.colorsBlue[3],
+        backgroundColor: colors.colorsBlue[3],
+        color: colors.colorsPrendaWhite,
+        '&:hover': {
+          borderColor: colors.colorsBlue[4],
+          backgroundColor: colors.colorsBlue[4],
+        },
+      },
+      '&.SparkButton-variantOutlined': {
+        borderColor: colors.colorsPrendaMediumGrey,
+        backgroundColor: colors.colorsPrendaWhite,
+        color: colors.colorsBlue[3],
+        '&:hover': {
+          backgroundColor: colors.colorsPrendaLightGrey,
+        },
+      },
+      '&.SparkButton-variantFlat': {
+        borderColor: 'transparent',
+        backgroundColor: 'transparent',
+        color: colors.colorsBlue[3],
+        '&:hover': {
+          color: colors.colorsBlue[4],
+        },
+        '&.Mui-focusVisible': {
+          borderColor: colors.colorsBlue[3],
+          backgroundColor: colors.colorsBlue[1],
+        },
+      },
+      // Y-paddings are subtracted by 2 to account for border-width
+      '&.SparkButton-sizeLarge': {
+        fontSize: '1.125rem', // 18px
+        lineHeight: '1.25rem', // 20px
+        padding: '.75rem 1rem', // 12px (accounting for 2px border width) 16px
+      },
+      '&.SparkButton-sizeMedium': {
+        fontSize: '1rem', // 16px
+        lineHeight: '1.25rem', // 20px
+        padding: '.25rem 1rem', // 4px (accounting for 2px border width) 16px
+      },
+      '&.SparkButton-sizeSmall': {
+        fontSize: '0.75rem', // 12px
+        lineHeight: '0.75rem', // 12px
+        padding: '.25rem 1rem', // 4px (accounting for 2px border width) 16px
+      },
+      '& .SparkButton-content': {
+        // taken from Mui
+        width: '100%', // Ensure the correct width for iOS Safari
+        display: 'inherit',
+        alignItems: 'inherit',
+        justifyContent: 'inherit',
+        '& .SparkButton-startIcon': {
+          display: 'inherit',
+          marginRight: '.5rem', // 8px
+        },
+        '& .SparkButton-endIcon': {
+          display: 'inherit',
+          marginLeft: '.5rem', // 8px
+        },
+        '& .SparkButton-iconSizeSmall': {
+          fontSize: '1rem', // 16px
+        },
+        '& .SparkButton-iconSizeMedium': {
+          fontSize: '1.25rem', // 20px
+        },
+        '& .SparkButton-iconSizeLarge': {
+          fontSize: '1.5rem', // 24px
+        },
+      },
+    },
+  },
+};
