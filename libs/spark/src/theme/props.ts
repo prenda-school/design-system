6fbbdd3dad140af726eee62b406e25cfbcbfc342
William Kelley <williamkelley9@gmail.com>
2021-07-01 14:56:29 -0400

    refactor(theme): move props to own file

---
+++ libs/spark/src/theme/props.ts
@@ -0,0 +1,17 @@
+import { MuiButtonBasePropOverrides } from '../ButtonBase';
+import { MuiCardPropOverrides } from '../Card';
+import { MuiCheckboxPropOverrides } from '../Checkbox';
+import { MuiInputPropOverrides } from '../Input';
+import { MuiMenuPropOverrides } from '../Menu';
+import { MuiRadioPropOverrides } from '../Radio';
+import { MuiSvgIconPropOverride } from '../SvgIcon';
+
+export const props = {
+  MuiButtonBase: MuiButtonBasePropOverrides,
+  MuiCard: MuiCardPropOverrides,
+  MuiCheckbox: MuiCheckboxPropOverrides,
+  MuiInput: MuiInputPropOverrides,
+  MuiMenu: MuiMenuPropOverrides,
+  MuiRadio: MuiRadioPropOverrides,
+  MuiSvgIcon: MuiSvgIconPropOverride,
+};
--- libs/spark/src/theme/theme.ts
+++ libs/spark/src/theme/theme.ts
@@ -1,30 +1,16 @@
 import { createMuiTheme, ThemeOptions } from '@material-ui/core';
 import { overrides } from './overrides';
 import { palette } from './palette';
+import { props } from './props';
 import { shadows } from './shadows';
 import { typography } from './typography';
-import { MuiButtonBasePropOverrides } from '../ButtonBase';
-import { MuiCardPropOverrides } from '../Card';
-import { MuiCheckboxPropOverrides } from '../Checkbox';
-import { MuiInputPropOverrides } from '../Input';
-import { MuiMenuPropOverrides } from '../Menu';
-import { MuiRadioPropOverrides } from '../Radio';
-import { MuiSvgIconPropOverride } from '../SvgIcon';
 
 export const prendaTheme: ThemeOptions = {
   typography,
   overrides,
   shadows,
   palette,
-  props: {
-    MuiButtonBase: MuiButtonBasePropOverrides,
-    MuiCard: MuiCardPropOverrides,
-    MuiCheckbox: MuiCheckboxPropOverrides,
-    MuiInput: MuiInputPropOverrides,
-    MuiMenu: MuiMenuPropOverrides,
-    MuiRadio: MuiRadioPropOverrides,
-    MuiSvgIcon: MuiSvgIconPropOverride,
-  },
+  props,
 };
 
 export const muiTheme = createMuiTheme(prendaTheme);
