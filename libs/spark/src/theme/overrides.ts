318ee4c1ed0af67de3a7194bf4341744a494ef92
William Kelley <williamkelley9@gmail.com>
2021-07-01 14:55:24 -0400

    refactor(theme): move overrides to own file

---
+++ libs/spark/src/theme/overrides.ts
@@ -0,0 +1,39 @@
+import { MuiButtonBaseStyleOverrides } from '../ButtonBase';
+import { MuiCardStyleOverrides } from '../Card';
+import { MuiCardContentStyleOverrides } from '../CardContent';
+import { MuiCardActionsStyleOverrides } from '../CardActions';
+import { MuiCheckboxStyleOverrides } from '../Checkbox';
+import { MuiFormControlLabelStyleOverrides } from '../FormControlLabel';
+import { MuiFormHelperTextStyleOverrides } from '../FormHelperText';
+import { MuiInputStyleOverrides } from '../Input';
+import { MuiInputLabelStyleOverrides } from '../InputLabel';
+import { MuiMenuStyleOverrides } from '../Menu';
+import { MuiMenuItemStyleOverrides } from '../MenuItem';
+import { MuiRadioStyleOverrides } from '../Radio';
+import { MuiSvgIconStyleOverrides } from '../SvgIcon';
+import { fontFaces, typography } from './typography';
+
+export const overrides = {
+  MuiButtonBase: MuiButtonBaseStyleOverrides,
+  MuiCssBaseline: {
+    '@global': {
+      body: {
+        fontFamily: typography.fontFamily,
+        fontSize: typography.fontSize,
+      },
+      '@font-face': fontFaces,
+    },
+    MuiCard: MuiCardStyleOverrides,
+    MuiCardContent: MuiCardContentStyleOverrides,
+    MuiCardActions: MuiCardActionsStyleOverrides,
+    MuiCheckbox: MuiCheckboxStyleOverrides,
+    MuiFormControlLabel: MuiFormControlLabelStyleOverrides,
+    MuiFormHelperText: MuiFormHelperTextStyleOverrides,
+    MuiInput: MuiInputStyleOverrides,
+    MuiInputLabel: MuiInputLabelStyleOverrides,
+    MuiMenu: MuiMenuStyleOverrides,
+    MuiMenuItem: MuiMenuItemStyleOverrides,
+    MuiRadio: MuiRadioStyleOverrides,
+    MuiSvgIcon: MuiSvgIconStyleOverrides,
+  },
+};
--- libs/spark/src/theme/theme.ts
+++ libs/spark/src/theme/theme.ts
@@ -1,53 +1,19 @@
 import { createMuiTheme, ThemeOptions } from '@material-ui/core';
+import { overrides } from './overrides';
 import { palette } from './palette';
 import { shadows } from './shadows';
-import { typography, fontFaces } from './typography';
-import {
-  MuiButtonBasePropOverrides,
-  MuiButtonBaseStyleOverrides,
-} from '../ButtonBase';
-import { MuiCardPropOverrides, MuiCardStyleOverrides } from '../Card';
-import { MuiCardContentStyleOverrides } from '../CardContent';
-import { MuiCardActionsStyleOverrides } from '../CardActions';
-import {
-  MuiCheckboxPropOverrides,
-  MuiCheckboxStyleOverrides,
-} from '../Checkbox';
-import { MuiFormControlLabelStyleOverrides } from '../FormControlLabel';
-import { MuiFormHelperTextStyleOverrides } from '../FormHelperText';
-import { MuiInputPropOverrides, MuiInputStyleOverrides } from '../Input';
-import { MuiInputLabelStyleOverrides } from '../InputLabel';
-import { MuiMenuPropOverrides, MuiMenuStyleOverrides } from '../Menu';
-import { MuiMenuItemStyleOverrides } from '../MenuItem';
-import { MuiRadioPropOverrides, MuiRadioStyleOverrides } from '../Radio';
-import { MuiSvgIconPropOverride, MuiSvgIconStyleOverrides } from '../SvgIcon';
+import { typography } from './typography';
+import { MuiButtonBasePropOverrides } from '../ButtonBase';
+import { MuiCardPropOverrides } from '../Card';
+import { MuiCheckboxPropOverrides } from '../Checkbox';
+import { MuiInputPropOverrides } from '../Input';
+import { MuiMenuPropOverrides } from '../Menu';
+import { MuiRadioPropOverrides } from '../Radio';
+import { MuiSvgIconPropOverride } from '../SvgIcon';
 
 export const prendaTheme: ThemeOptions = {
   typography,
-  overrides: {
-    MuiButtonBase: MuiButtonBaseStyleOverrides,
-    MuiCssBaseline: {
-      '@global': {
-        body: {
-          fontFamily: typography.fontFamily,
-          fontSize: typography.fontSize,
-        },
-        '@font-face': fontFaces,
-      },
-    },
-    MuiCard: MuiCardStyleOverrides,
-    MuiCardContent: MuiCardContentStyleOverrides,
-    MuiCardActions: MuiCardActionsStyleOverrides,
-    MuiCheckbox: MuiCheckboxStyleOverrides,
-    MuiFormControlLabel: MuiFormControlLabelStyleOverrides,
-    MuiFormHelperText: MuiFormHelperTextStyleOverrides,
-    MuiInput: MuiInputStyleOverrides,
-    MuiInputLabel: MuiInputLabelStyleOverrides,
-    MuiMenu: MuiMenuStyleOverrides,
-    MuiMenuItem: MuiMenuItemStyleOverrides,
-    MuiRadio: MuiRadioStyleOverrides,
-    MuiSvgIcon: MuiSvgIconStyleOverrides,
-  },
+  overrides,
   shadows,
   palette,
   props: {
