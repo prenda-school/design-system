60a893c75b2ab2275c92c02ed0b65bc8e4053a9d
William Kelley <williamkelley9@gmail.com>
2021-06-30 21:51:11 -0400

    feat(palette): move greys under palette.grey

--- libs/spark/src/theme/palette.ts
+++ libs/spark/src/theme/palette.ts
@@ -1,3 +1,104 @@
+import { Color as MuiColor } from '@material-ui/core';
+import {
+  TypeText as MuiTypeText,
+  TypeBackground as MuiTypeBackground,
+  PaletteOptions as MuiPaletteOptions,
+} from '@material-ui/core/styles/createPalette';
+
+// Customized types
+interface TypeText extends MuiTypeText {
+  onDark: string;
+  onDarkLowContrast: string;
+  onLight: string;
+  onLightLowContrast: string;
+}
+
+interface TypeBackground extends MuiTypeBackground {
+  navy: string;
+  blue: string;
+  lightGrey: string;
+  lightBlue: string;
+  white: string;
+}
+
+interface Color extends MuiColor {
+  lighter: '#f5f5f5';
+  light: '#f0f1f2';
+  medium: '#e1e3e5';
+  dark: '#d2d4d6';
+}
+
+// Custom types
+interface PaletteTertiaryColor {
+  1: string;
+  2: string;
+  3: string;
+  4: string;
+  5: string;
+}
+
+interface TypeBrand {
+  blue: string;
+  lightBlue: string;
+  orange: string;
+  peach: string;
+}
+
+interface PaletteOptions extends MuiPaletteOptions {
+  grey: Color;
+  text: TypeText;
+  background: TypeBackground;
+  brand: TypeBrand;
+  red: PaletteTertiaryColor;
+  orange: PaletteTertiaryColor;
+  yellow: PaletteTertiaryColor;
+  green: PaletteTertiaryColor;
+  blue: PaletteTertiaryColor;
+  purple: PaletteTertiaryColor;
+}
+
+// Re-declare overridden interface so that modules outside of this one can
+//  recognize our customizations
+declare module '@material-ui/core/index' {
+  // Only need to declare custom properties here; extending will throw
+  interface Color {
+    lighter: string;
+    light: string;
+    medium: string;
+    dark: string;
+  }
+}
+
+declare module '@material-ui/core/styles/createPalette' {
+  // Only need to declare custom properties here; extending will throw
+  interface TypeText {
+    onDark: string;
+    onDarkLowContrast: string;
+    onLight: string;
+    onLightLowContrast: string;
+  }
+
+  interface TypeBackground {
+    navy: string;
+    blue: string;
+    lightGrey: string;
+    lightBlue: string;
+    white: string;
+  }
+
+  interface Palette {
+    text: TypeText;
+    background: TypeBackground;
+    brand: TypeBrand;
+    red: PaletteTertiaryColor;
+    orange: PaletteTertiaryColor;
+    yellow: PaletteTertiaryColor;
+    green: PaletteTertiaryColor;
+    blue: PaletteTertiaryColor;
+    purple: PaletteTertiaryColor;
+  }
+}
+
 // Palette properties with repeatedly used values are extracted
 const red = {
   1: '#f7d2da',
@@ -6,6 +107,7 @@ const red = {
   4: '#851b2c',
   5: '#520a18',
 };
+
 const orange = {
   1: '#ffb78f',
   2: '#ff7f4d',
@@ -13,6 +115,7 @@ const orange = {
   4: '#a34814',
   5: '#5c2704',
 };
+
 const yellow = {
   1: '#fae6b2',
   2: '#f7d054',
@@ -20,6 +123,7 @@ const yellow = {
   4: '#8f691d',
   5: '#4c3109',
 };
+
 const green = {
   1: '#b8f0d4',
   2: '#4aa784',
@@ -27,6 +131,7 @@ const green = {
   4: '#0c664e',
   5: '#084237',
 };
+
 const blue = {
   1: '#d7f3ff',
   2: '#498dcc',
@@ -34,6 +139,7 @@ const blue = {
   4: '#0a4872',
   5: '#072e44',
 };
+
 const purple = {
   1: '#d9d8fb',
   2: '#8070f6',
@@ -41,30 +147,51 @@ const purple = {
   4: '#452ea3',
   5: '#271466',
 };
+
 const brand = {
   blue: blue[4],
   lightBlue: blue[1],
   orange: '#f34700',
   peach: orange[1],
 };
-const neutral = {
-  navy: blue[5],
-  white: '#fff',
-  lighterGrey: '#f5f5f5',
-  lightGrey: '#f0f1f2',
-  mediumGrey: '#e1e3e5',
-  darkGrey: '#d2d4d6',
+
+const grey = {
+  lighter: '#f5f5f5' as const,
+  light: '#f0f1f2' as const,
+  medium: '#e1e3e5' as const,
+  dark: '#d2d4d6' as const,
+  // unchanged from Mui
+  50: '#fafafa',
+  100: '#f5f5f5',
+  200: '#eeeeee',
+  300: '#e0e0e0',
+  400: '#bdbdbd',
+  500: '#9e9e9e',
+  600: '#757575',
+  700: '#616161',
+  800: '#424242',
+  900: '#212121',
+  A100: '#d5d5d5',
+  A200: '#aaaaaa',
+  A400: '#303030',
+  A700: '#616161',
 };
+
 const text = {
-  onDark: neutral.lightGrey,
-  onDarkLowContrast: `${neutral.lightGrey}b8`, // 72% opacity
+  onDark: grey.light,
+  onDarkLowContrast: `${grey.light}b8`, // 72% opacity
   onLight: blue[5],
   onLightLowContrast: `${blue[5]}b8`, // 72% opacity
+  // unchanged from Mui
+  primary: 'rgba(0, 0, 0, 0.87)',
+  secondary: 'rgba(0, 0, 0, 0.54)',
+  disabled: 'rgba(0, 0, 0, 0.38)',
+  hint: 'rgba(0, 0, 0, 0.38)',
 };
 
 // Only customizations are specified, view all other default theme.palette properties
 //  at https://material-ui.com/customization/default-theme/?expand-path=$.palette
-export const palette = {
+export const palette: PaletteOptions = {
   // Mui default properties, with only necessary values changed
   error: {
     main: red[3],
@@ -83,17 +210,20 @@ export const palette = {
     main: green[3],
     contrastText: text.onDark,
   },
+  grey,
   text,
   background: {
-    navy: neutral.navy,
+    navy: blue[5],
     blue: brand.blue,
-    lightGrey: neutral.lightGrey,
+    lightGrey: grey.light,
     lightBlue: brand.lightBlue,
-    white: neutral.white,
+    white: grey[0],
+    // unchanged from Mui
+    paper: '#fff',
+    default: '#fafafa',
   },
   // custom properties
   brand,
-  neutral,
   red,
   orange,
   yellow,
