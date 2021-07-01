fa51a60b8a2944df5902ae4a4363cb06970c420a
William Kelley <williamkelley9@gmail.com>
2021-07-01 09:42:59 -0400

    feat(palette): move greys under grey, remove whites, navy, neutrals (#105)
    * feat(palette): move greys under palette.grey
    
    * make uncustomized mui defaults optional in custom types
    
    * remove first-person in comments, document Figma relation
    
    * remove augmentations
    
    * remove background.white
    
    - accessible by common.white

--- libs/spark/src/theme/palette.ts
+++ libs/spark/src/theme/palette.ts
@@ -6,6 +6,7 @@ const red = {
   4: '#851b2c',
   5: '#520a18',
 };
+
 const orange = {
   1: '#ffb78f',
   2: '#ff7f4d',
@@ -13,6 +14,7 @@ const orange = {
   4: '#a34814',
   5: '#5c2704',
 };
+
 const yellow = {
   1: '#fae6b2',
   2: '#f7d054',
@@ -20,6 +22,7 @@ const yellow = {
   4: '#8f691d',
   5: '#4c3109',
 };
+
 const green = {
   1: '#b8f0d4',
   2: '#4aa784',
@@ -27,13 +30,15 @@ const green = {
   4: '#0c664e',
   5: '#084237',
 };
+
 const blue = {
   1: '#d7f3ff',
   2: '#498dcc',
   3: '#2967a6',
   4: '#0a4872',
-  5: '#072e44',
+  5: '#072e44', // Also "Neutral Navy" in Figma
 };
+
 const purple = {
   1: '#d9d8fb',
   2: '#8070f6',
@@ -41,23 +46,24 @@ const purple = {
   4: '#452ea3',
   5: '#271466',
 };
+
 const brand = {
   blue: blue[4],
   lightBlue: blue[1],
   orange: '#f34700',
   peach: orange[1],
 };
-const neutral = {
-  navy: blue[5],
-  white: '#fff',
-  lighterGrey: '#f5f5f5',
-  lightGrey: '#f0f1f2',
-  mediumGrey: '#e1e3e5',
-  darkGrey: '#d2d4d6',
+
+const grey = {
+  lighter: '#f5f5f5' as const, // "Neutral Lighter Grey" in Figma
+  light: '#f0f1f2' as const, // "Neutral Light Grey" in Figma
+  medium: '#e1e3e5' as const, // "Neutral Medium Grey" in Figma
+  dark: '#d2d4d6' as const, // "Neutral Dark Grey" in Figma
 };
+
 const text = {
-  onDark: neutral.lightGrey,
-  onDarkLowContrast: `${neutral.lightGrey}b8`, // 72% opacity
+  onDark: grey.light,
+  onDarkLowContrast: `${grey.light}b8`, // 72% opacity
   onLight: blue[5],
   onLightLowContrast: `${blue[5]}b8`, // 72% opacity
 };
@@ -65,7 +71,7 @@ const text = {
 // Only customizations are specified, view all other default theme.palette properties
 //  at https://material-ui.com/customization/default-theme/?expand-path=$.palette
 export const palette = {
-  // Mui default properties, with only necessary values changed
+  // Mui default properties, only customizations specified
   error: {
     main: red[3],
     contrastText: text.onDark,
@@ -74,7 +80,7 @@ export const palette = {
     main: yellow[2],
     contrastText: text.onLight,
   },
-  // "Primary Action" -> info
+  // "Primary Action" in Figma
   info: {
     main: blue[3],
     contrastText: text.onDark,
@@ -83,17 +89,16 @@ export const palette = {
     main: green[3],
     contrastText: text.onDark,
   },
+  grey,
   text,
   background: {
-    navy: neutral.navy,
+    navy: blue[5],
     blue: brand.blue,
-    lightGrey: neutral.lightGrey,
+    lightGrey: grey.light,
     lightBlue: brand.lightBlue,
-    white: neutral.white,
   },
   // custom properties
   brand,
-  neutral,
   red,
   orange,
   yellow,
