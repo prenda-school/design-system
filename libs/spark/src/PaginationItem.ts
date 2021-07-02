eb3b00d1bf0cf728df2a117aaa88bf2263452559
William Kelley <williamkelley9@gmail.com>
2021-07-02 15:23:57 -0400

    better match ellipsis styling

--- libs/spark/src/PaginationItem.ts
+++ libs/spark/src/PaginationItem.ts
@@ -42,15 +42,34 @@ export const MuiPaginationItemStyleOverrides = {
     minWidth: '3rem',
   },
   ellipsis: {
-    height: '3rem',
-    borderRadius: 0,
-    backgroundColor: palette.common.white,
-    border: `solid 2px ${palette.grey.medium}`,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    fontWeight: 900,
-    fontSize: '1.25rem',
-    color: palette.text.onLightLowContrast,
+    // the internal ellipsis is the actual character, but it's
+    // "letter spacing" is too small (not actually controlled by letter-spacing
+    // because it's a single character), so replace with three periods
+    '&::after': {
+      height: '3rem',
+      borderRadius: 0,
+      backgroundColor: palette.common.white,
+      border: `solid 2px ${palette.grey.medium}`,
+      display: 'flex',
+      justifyContent: 'center',
+      fontWeight: 900,
+      fontSize: '1.5rem',
+      lineHeight: '1.75rem',
+      color: palette.text.onLightLowContrast,
+      // extra to shift it down
+      alignItems: 'flex-end',
+      paddingBottom: '.375rem',
+      // extra for this being ::after
+      visibility: 'visible',
+      position: 'absolute',
+      inset: 'auto',
+      content: "'...'",
+      letterSpacing: '1px',
+      padding: '0 10px',
+      minWidth: '3rem',
+    },
   },
 };
