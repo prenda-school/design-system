c98c20e5ff8e7673af405b8727765ae9aced3138
William Kelley <williamkelley9@gmail.com>
2021-07-02 23:58:40 -0400

    feat(Radio)!: remove custom combo with FormControlLabel
    - remove test because we no longer export a component

--- libs/spark/stories/radio.stories.tsx
+++ libs/spark/stories/radio.stories.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 import { Meta } from '@storybook/react/types-6-0';
-import { Radio, RadioProps } from '../src';
+import { Radio, RadioProps } from '@material-ui/core';
 
 export default {
   title: 'prenda-spark/Radio',
   component: Radio,
   parameters: { actions: { handles: ['change'] } },
   argTypes: {
-    label: { control: 'text' },
     checked: { control: 'boolean' },
     disabled: { control: 'boolean' },
   },
-  args: {
-    label: 'Radio button label',
-  },
+  args: { },
 } as Meta;
 
 const Template: Story<RadioProps> = (args) => <Radio {...args} />;
-export const ConfigurableInput = Template.bind({});
+
+export const Configurable = Template.bind({});
