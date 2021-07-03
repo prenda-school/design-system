cd796bbb977138b7c49dcdaac401269791c40a91
William Kelley <williamkelley9@gmail.com>
2021-07-03 18:25:53 -0400

    feat(Checkbox)!: decouple from FormControlLabel

--- libs/spark/src/index.ts
+++ libs/spark/src/index.ts
@@ -1,7 +1,5 @@
 export { Button } from './Button';
 export type { ButtonProps } from './Button';
-export { Checkbox } from './Checkbox';
-export type { CheckboxProps } from './Checkbox';
 export {
   DropdownContext,
   DropdownButton,
