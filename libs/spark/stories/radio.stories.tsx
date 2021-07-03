f79e43c84f5e773bc9a6e94f7a2f80bec5f45aab
William Kelley <williamkelley9@gmail.com>
2021-07-03 00:44:09 -0400

    add label support back to story

--- libs/spark/stories/radio.stories.tsx
+++ libs/spark/stories/radio.stories.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 import { Meta } from '@storybook/react/types-6-0';
-import { Radio, RadioProps } from '@material-ui/core';
+import { FormControlLabel, Radio, RadioProps } from '@material-ui/core';
 
 export default {
   title: 'prenda-spark/Radio',
   component: Radio,
   parameters: { actions: { handles: ['change'] } },
   argTypes: {
+    label: { control: 'input' },
     checked: { control: 'boolean' },
     disabled: { control: 'boolean' },
   },
-  args: { },
+  args: {
+    label: 'Label',
+  },
 } as Meta;
 
-const Template: Story<RadioProps> = (args) => <Radio {...args} />;
+const Template: Story = ({ label, ...args }) => (
+  <FormControlLabel label={label} control={<Radio {...args} />} />
+);
 
 export const Configurable = Template.bind({});
