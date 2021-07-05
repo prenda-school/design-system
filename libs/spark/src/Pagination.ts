e4ea103fa185d42dc47bb5a5059516e1fc12f3cf
William Kelley <williamkelley9@gmail.com>
2021-07-05 15:54:24 -0400

    simplify style values

--- libs/spark/src/Pagination.ts
+++ libs/spark/src/Pagination.ts
@@ -1,30 +1,30 @@
 export const MuiPaginationStyleOverrides = {
   ul: {
     '& > li:first-child > .MuiPaginationItem-root': {
-      borderTopLeftRadius: '8px',
-      borderBottomLeftRadius: '8px'
+      borderTopLeftRadius: 8,
+      borderBottomLeftRadius: 8,
     },
     '& > li:last-child > .MuiPaginationItem-root': {
-      borderTopRightRadius: '8px',
-      borderBottomRightRadius: '8px'
+      borderTopRightRadius: 8,
+      borderBottomRightRadius: 8,
     },
     '& > li:not(:last-child) > .MuiPaginationItem-root': {
-      marginRight: '-1px'
+      marginRight: -1,
     },
     '& > li:not(:first-child) > .MuiPaginationItem-root': {
-      marginLeft: '-1px'
+      marginLeft: -1,
     },
     '& > li > .MuiPaginationItem-root.Mui-selected': {
-      zIndex: 2
+      zIndex: 2,
     },
     '& > li > .MuiPaginationItem-root:hover, .MuiPaginationItem-root:focus, button.Mui-focusVisible': {
-      zIndex: 1
-    }
-  }
+      zIndex: 1,
+    },
+  },
 };
 
 export const MuiPaginationDefaultProps = {
   variant: 'outlined' as const,
   shape: 'rounded' as const,
-  size: 'large' as const
+  size: 'large' as const,
 };
