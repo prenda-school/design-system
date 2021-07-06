114ad283ef573732ad6fb8c4844cc5636569c59f
William Kelley <williamkelley9@gmail.com>
2021-07-06 16:43:23 -0400

    feat(Input)!: remove custom implementation

--- libs/spark/src/Input.tsx
+++ libs/spark/src/Input.tsx
@@ -1,23 +1,4 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
-import clsx from 'clsx';
-import {
-  Input,
-  InputLabel,
-  FormHelperText,
-  InputProps as MuiInputProps,
-  InputLabelProps as MuiInputLabelProps,
-  Theme,
-} from '@material-ui/core/';
-import { FormHelperTextProps as MuiFormHelperTextProps } from '@material-ui/core/FormHelperText';
 import { palette } from './styles/palette';
-export interface InputProps extends MuiInputProps {
-  success?: boolean;
-  label?: string;
-  InputLabelProps?: MuiInputLabelProps;
-  helperText?: string;
-  FormHelperTextProps?: MuiFormHelperTextProps;
-}
 
 export const MuiInputDefaultProps = {
   disableUnderline: true,
@@ -35,17 +16,14 @@ export const MuiInputStyleOverrides = {
     padding: '.75rem 1rem',
     fontSize: '1rem', // 16px
     lineHeight: '1.125rem', // 18px
-    '&.SparkInput-success': {
-      borderColor: palette.green[3],
-      boxShadow: `0 0 0 4px ${palette.green[1]}`,
-    },
-    '&$focused': {
+    '&$focused, &:focus': {
       borderColor: palette.blue[3],
       boxShadow: `0 0 0 4px ${palette.blue[1]}`,
       backgroundColor: palette.common.white,
-      '& .MuiInput-input': {
-        color: palette.text.onLight,
-      },
+    },
+    '&.SparkInput-success': {
+      borderColor: palette.green[3],
+      boxShadow: `0 0 0 4px ${palette.green[1]}`,
     },
     '&$error': {
       borderColor: palette.red[3],
@@ -58,56 +36,16 @@ export const MuiInputStyleOverrides = {
   },
   input: {
     padding: 0,
+    color: palette.text.onLight,
     '&::placeholder': {
-      opacity: 1,
       color: palette.text.onLightLowContrast,
+      opacity: 1,
+    },
+    '&$disabled': {
+      opacity: 0.42,
+      '&::placeholder': {
+        opacity: 0.42,
+      },
     },
   },
 };
-
-const SparkInput: FC<InputProps> = (props) => {
-  const {
-    id,
-    label,
-    helperText,
-    disabled,
-    success,
-    className,
-    InputLabelProps,
-    FormHelperTextProps,
-    ...other
-  } = props;
-
-  // TODO: throw a helpful error when detecting that both error & success props
-  // are `true` -- look to MUI's method of throwing errors as an inspiration.
-  if (process.env.NODE_ENV !== 'production' && props.error && props.success) {
-    console.warn(
-      '@prenda/spark SparkInput detected both error and success props as `true`'
-    );
-  }
-
-  return (
-    <>
-      {label ? (
-        <InputLabel htmlFor={id} {...InputLabelProps}>
-          {label}
-        </InputLabel>
-      ) : null}
-      <Input
-        id={id}
-        className={clsx(className, {
-          'SparkInput-success': success,
-        })}
-        disabled={disabled}
-        {...other}
-      />
-      {helperText ? (
-        <FormHelperText disabled={disabled} {...FormHelperTextProps}>
-          {helperText}
-        </FormHelperText>
-      ) : null}
-    </>
-  );
-};
-
-export { SparkInput as Input };
