491802ebc0108cb10229d70a516ac6904dc2a0cc
Scott Lingner <scott@prenda.co>
2021-07-07 09:48:38 -0700

    used color from styledAppBar

--- libs/spark/src/Navbar.tsx
+++ libs/spark/src/Navbar.tsx
@@ -6,22 +6,21 @@ import { palette } from './styles/palette';
 export interface NavBarProps extends AppBarProps {
   color?: 'default';
 }
+const StyledAppBar = withStyles({
+  root: {
+    borderBottom: `2px solid ${palette.grey.medium}`,
+    fontSize: '18px',
+    color: palette.text.onLightLowContrast,
+  },
+})(AppBar);
 
 export const NavBar = ({ color = 'default', ...other }: NavBarProps) => {
-  const PrendaAppBar = withStyles({
-    root: {
-      borderBottom: `2px solid ${palette.grey.medium}`,
-      fontSize: '18px',
-      color: palette.text.onLightLowContrast,
-    },
-  })(AppBar);
-
   return (
-    <PrendaAppBar position="fixed" color="inherit" elevation={0}>
+    <StyledAppBar position="fixed" color={co} elevation={0}>
       <Toolbar>
         <PrendaMonogramDark fontSize="62px" />
         {other.children}
       </Toolbar>
-    </PrendaAppBar>
+    </StyledAppBar>
   );
 };
