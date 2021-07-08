e7a002647f72507dfcc897d1a16c802bad8cb428
Scott Lingner <scott@prenda.co>
2021-07-08 16:08:13 -0700

    added router to navbar spec

--- libs/spark/test/navbar.spec.tsx
+++ libs/spark/test/navbar.spec.tsx
@@ -2,18 +2,20 @@ import { render } from '@testing-library/react';
 import 'jest-styled-components';
 import { SparkThemeProvider, NavBar, NavBarItem } from '../src';
 import { QuestionIconDuotone } from '../src/icons';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 describe('Button', () => {
   it('is truthy', () => {
     const { baseElement } = render(
       <SparkThemeProvider>
-        <NavBar>
-          <NavBarItem component={Link} to="/">
-            <QuestionIconDuotone />
-            <span>Dashboard</span>
-          </NavBarItem>
-        </NavBar>
+        <Router>
+          <NavBar>
+            <NavBarItem component={Link} to="/">
+              <QuestionIconDuotone />
+              <span>Dashboard</span>
+            </NavBarItem>
+          </NavBar>
+        </Router>
       </SparkThemeProvider>
     );
     expect(baseElement).toBeTruthy();
