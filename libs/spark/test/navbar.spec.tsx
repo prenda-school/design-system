098e1027dab03bacbe60addfd6d300c4783c1dcf
Scott Lingner <scott@prenda.co>
2021-07-08 16:06:55 -0700

    updated navbar test

--- libs/spark/test/navbar.spec.tsx
+++ libs/spark/test/navbar.spec.tsx
@@ -1,17 +1,18 @@
 import { render } from '@testing-library/react';
 import 'jest-styled-components';
-import { SparkThemeProvider, NavBar, NavbarLink } from '../src';
+import { SparkThemeProvider, NavBar, NavBarItem } from '../src';
 import { QuestionIconDuotone } from '../src/icons';
+import { Link } from 'react-router-dom';
 
 describe('Button', () => {
   it('is truthy', () => {
     const { baseElement } = render(
       <SparkThemeProvider>
         <NavBar>
-          <NavbarLink href="/">
+          <NavBarItem component={Link} to="/">
             <QuestionIconDuotone />
             <span>Dashboard</span>
-          </NavbarLink>
+          </NavBarItem>
         </NavBar>
       </SparkThemeProvider>
     );
