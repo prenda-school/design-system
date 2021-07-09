7483a55332c53ce443478743d697ca0cb9f9c700
Scott Lingner <scott@prenda.co>
2021-07-09 13:48:25 -0700

    updated spec for navbar

--- libs/spark/test/navbar.spec.tsx
+++ libs/spark/test/navbar.spec.tsx
@@ -2,20 +2,17 @@ import { render } from '@testing-library/react';
 import 'jest-styled-components';
 import { SparkThemeProvider, NavBar, NavBarItem } from '../src';
 import { QuestionIconDuotone } from '../src/icons';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 describe('Button', () => {
   it('is truthy', () => {
     const { baseElement } = render(
       <SparkThemeProvider>
-        <Router>
-          <NavBar>
-            <NavBarItem component={Link} to="/">
-              <QuestionIconDuotone />
-              <span>Dashboard</span>
-            </NavBarItem>
-          </NavBar>
-        </Router>
+        <NavBar>
+          <NavBarItem href="/">
+            <QuestionIconDuotone />
+            <span>Dashboard</span>
+          </NavBarItem>
+        </NavBar>
       </SparkThemeProvider>
     );
     expect(baseElement).toBeTruthy();
