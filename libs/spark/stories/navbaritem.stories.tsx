0bce643da60f6aa3f7eb878cdc01e9a42ac8ff3b
Scott Lingner <scott@prenda.co>
2021-07-09 16:00:30 -0700

    updated navbaritem story with imported meta and export default

--- libs/spark/stories/navbaritem.stories.tsx
+++ libs/spark/stories/navbaritem.stories.tsx
@@ -1,5 +1,13 @@
 import { NavBarItem } from '@prenda/spark';
 import { UsersIconDuotone } from '../src/icons';
+import { Meta } from '@storybook/react/types-6-0';
+
+export default {
+  title: 'prenda-spark/NavBarItem',
+  component: NavBarItem,
+  argTypes: {},
+  args: {},
+} as Meta;
 
 const Template = () => (
   <NavBarItem href="#" startIcon={<UsersIconDuotone />}>
