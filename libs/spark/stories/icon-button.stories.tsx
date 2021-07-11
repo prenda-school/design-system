fc7784ae5f8194e90067ad6e8491e8a8327e5e15
William Kelley <williamkelley9@gmail.com>
2021-07-11 00:20:16 -0400

    feat(IconButton): update stylings
    - expand stories

--- libs/spark/stories/icon-button.stories.tsx
+++ libs/spark/stories/icon-button.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { IconButton, IconButtonProps } from '../src';
 import { ChevronDownIconLine } from '../src/icons';
+import Box from '@material-ui/core/Box';
 
 export default {
   title: 'prenda-spark/IconButton',
@@ -9,37 +10,103 @@ export default {
   argTypes: {
     onClick: { actions: 'clicked' },
     variant: {
-      control: {
-        type: 'select',
-        options: ['solid', 'outlined', 'flat'],
-      },
+      control: 'select',
+      options: ['solid', 'outlined', 'flat'],
     },
     size: {
-      control: {
-        type: 'select',
-        options: ['large', 'medium'],
-      },
+      control: 'select',
+      options: ['large', 'medium'],
+    },
+    children: {
+      control: 'select',
+      options: ['ChevronDown'],
     },
     disabled: { control: 'boolean' },
-    children: {
-      control: {
-        type: 'select',
-        options: ['caret-down'],
-      },
-    },
   },
   args: {
     variant: 'solid',
-    size: 'medium',
-    children: 'caret-down',
+    size: 'large',
+    children: 'ChevronDown',
   },
 } as Meta;
 
 interface TemplateIconButtonProps extends IconButtonProps {
-  children?: 'caret-down';
+  children?: 'ChevronDown';
 }
 
 const Template = (args: TemplateIconButtonProps) => (
-  <IconButton {...args}>{<ChevronDownIconLine />}</IconButton>
+  <IconButton {...args}>
+    <ChevronDownIconLine />
+  </IconButton>
 );
-export const ConfigurableInput = Template.bind({});
+
+export const Configurable = Template.bind({});
+
+const GridContainer = (props) => (
+  <Box
+    m={1}
+    display="grid"
+    gridTemplateColumns="1fr 1fr 1fr"
+    gridGap="16px"
+    alignItems="end"
+    {...props}
+  />
+);
+
+const VariantAndSizeTemplate = (args) => (
+  <GridContainer>
+    <span>Variant / Size</span>
+    <span>Large</span>
+    <span>Medium</span>
+
+    <span>Solid</span>
+    <span>
+      <IconButton {...args} variant="solid" size="large">
+        <ChevronDownIconLine />
+      </IconButton>
+    </span>
+    <span>
+      <IconButton {...args} variant="solid" size="medium">
+        <ChevronDownIconLine />
+      </IconButton>
+    </span>
+
+    <span>Outlined</span>
+    <span>
+      <IconButton {...args} variant="outlined" size="large">
+        <ChevronDownIconLine />
+      </IconButton>
+    </span>
+    <span>
+      <IconButton {...args} variant="outlined" size="medium">
+        <ChevronDownIconLine />
+      </IconButton>
+    </span>
+
+    <span>Flat</span>
+    <span>
+      <IconButton {...args} variant="flat" size="large">
+        <ChevronDownIconLine />
+      </IconButton>
+    </span>
+    <span>
+      <IconButton {...args} variant="flat" size="medium">
+        <ChevronDownIconLine />
+      </IconButton>
+    </span>
+  </GridContainer>
+);
+
+export const VariantAndSize = VariantAndSizeTemplate.bind({});
+
+export const VariantAndSizeDisabled = VariantAndSizeTemplate.bind({});
+VariantAndSizeDisabled.args = { disabled: true };
+
+export const VariantAndSizeHover = VariantAndSizeTemplate.bind({});
+VariantAndSizeHover.parameters = { pseudo: { hover: true } };
+
+export const VariantAndSizeFocus = VariantAndSizeTemplate.bind({});
+VariantAndSizeFocus.parameters = { pseudo: { focus: true } };
+
+export const VariantAndSizeActive = VariantAndSizeTemplate.bind({});
+VariantAndSizeActive.parameters = { pseudo: { active: true } };
