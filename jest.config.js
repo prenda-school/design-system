79a8cf8b8f92a9a1fac4d45a28aefc009b3438c6
William Kelley <williamkelley9@gmail.com>
2021-07-13 10:23:13 -0400

    chore(nx): generate new project, spark-icons (@prenda/spark-icons) (#132)
    * chore(nx): fix npmScope
    
    * chore(nx): generate new project, spark-icons
    
    - publishable as @prenda/spark-icons
    
    * add extra rules

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/libs/spark'],
+  projects: ['<rootDir>/libs/spark', '<rootDir>/libs/spark-icons'],
 };
---
+++ libs/spark-icons/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'spark-icons',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/spark-icons',
+};
