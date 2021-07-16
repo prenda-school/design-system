879f8285a6b8ecaec50bf7ca16300467d308d0ac
William Kelley <williamkelley9@gmail.com>
2021-07-16 11:04:17 -0400

    feat(spark-icons): build to be path-import friendly without tree-shaking (#139)
    * add new scripts from Mui for an alternative build
    
    * chore(spark): better specify deps in package.json
    
    * fix babel config
    
    * change name from spark-icons-2 to spark-icons-alt

---
+++ libs/spark-icons/scripts/copy-files.js
@@ -0,0 +1,121 @@
+/* eslint-disable no-console */
+const path = require('path');
+const fse = require('fs-extra');
+const glob = require('glob');
+
+const packagePath = process.cwd();
+const buildPath = path.join(packagePath, './dist/libs/spark-icons-alt');
+const srcPath = path.join(packagePath, './libs/spark-icons/src');
+
+async function includeFileInBuild(file) {
+  const sourcePath = path.resolve(packagePath, file);
+  const targetPath = path.resolve(buildPath, path.basename(file));
+  await fse.copy(sourcePath, targetPath);
+  console.log(`Copied ${sourcePath} to ${targetPath}`);
+}
+
+/**
+ * Puts a package.json into every immediate child directory of rootDir.
+ * That package.json contains information about esm for bundlers so that imports
+ * like import Typography from '@material-ui/core/Typography' are tree-shakeable.
+ *
+ * It also tests that an this import can be used in typescript by checking
+ * if an index.d.ts is present at that path.
+ *
+ * @param {string} rootDir
+ */
+async function createModulePackages({ from, to }) {
+  const directoryPackages = glob
+    .sync('*/index.js', { cwd: from })
+    .map(path.dirname);
+
+  await Promise.all(
+    directoryPackages.map(async (directoryPackage) => {
+      const packageJson = {
+        sideEffects: false,
+        module: path.posix.join('../esm', directoryPackage, 'index.js'),
+        typings: './index.d.ts',
+      };
+      const packageJsonPath = path.join(to, directoryPackage, 'package.json');
+
+      const [typingsExist] = await Promise.all([
+        fse.exists(path.join(to, directoryPackage, 'index.d.ts')),
+        fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2)),
+      ]);
+
+      if (!typingsExist) {
+        throw new Error(`index.d.ts for ${directoryPackage} is missing`);
+      }
+
+      return packageJsonPath;
+    })
+  );
+}
+
+async function typescriptCopy({ from, to }) {
+  if (!(await fse.exists(to))) {
+    console.warn(`path ${to} does not exists`);
+    return [];
+  }
+
+  const files = glob.sync('**/*.d.ts', { cwd: from });
+  const cmds = files.map((file) =>
+    fse.copy(path.resolve(from, file), path.resolve(to, file))
+  );
+  return Promise.all(cmds);
+}
+
+async function createPackageFile() {
+  const packageData = await fse.readFile(
+    path.resolve(packagePath, './libs/spark-icons/package.json'),
+    'utf8'
+  );
+  const {
+    nyc,
+    scripts,
+    devDependencies,
+    workspaces,
+    ...packageDataOther
+  } = JSON.parse(packageData);
+  const newPackageData = {
+    ...packageDataOther,
+    private: false,
+    main: './index.js',
+    module: './esm/index.js',
+    typings: './index.d.ts',
+  };
+  const targetPath = path.resolve(buildPath, './package.json');
+
+  await fse.writeFile(
+    targetPath,
+    JSON.stringify(newPackageData, null, 2),
+    'utf8'
+  );
+  console.log(`Created package.json in ${targetPath}`);
+
+  return newPackageData;
+}
+
+async function run() {
+  try {
+    await createPackageFile();
+
+    await Promise.all(
+      [
+        './libs/spark-icons/README.md',
+        // './libs/spark-icons/CHANGELOG.md',
+        // './libs/spark-icons/LICENSE',
+      ].map((file) => includeFileInBuild(file))
+    );
+
+    // TypeScript
+    await typescriptCopy({ from: srcPath, to: buildPath });
+
+    await createModulePackages({ from: srcPath, to: buildPath });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+run();
