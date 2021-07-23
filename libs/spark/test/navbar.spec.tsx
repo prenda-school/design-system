19d6df8bf8debc2ad0b22679652ce862a62ab6fd
Scott Lingner <56003456+slingner@users.noreply.github.com>
2021-07-23 11:08:10 -0700

    Update libs/spark/test/navbar.spec.tsx
    Co-authored-by: William Kelley <williamkelley9@gmail.com>

--- libs/spark/test/navbar.spec.tsx
+++ libs/spark/test/navbar.spec.tsx
@@ -3,7 +3,7 @@ import 'jest-styled-components';
 import { SparkThemeProvider, NavBar, NavBarButton } from '../src';
 import QuestionDuotone from '@prenda/spark-icons/QuestionDuotone';
 
-describe('Button', () => {
+describe('NavBar', () => {
   it('is truthy', () => {
     const { baseElement } = render(
       <SparkThemeProvider>
