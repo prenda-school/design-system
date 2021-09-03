e9182017e4e1b32d3f66490bd2794949bb405594
William Kelley <williamkelley9@gmail.com>
2021-09-03 14:16:52 -0400

    fix: replace createMuiTheme with createTheme

--- libs/spark/src/styles/theme.ts
+++ libs/spark/src/styles/theme.ts
@@ -1,11 +1,11 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core';
 import { overrides } from './overrides';
 import { palette } from './palette';
 import { props } from './props';
 import { shadows } from './shadows';
 import { typography } from './typography';
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
   overrides,
   palette,
   props,
