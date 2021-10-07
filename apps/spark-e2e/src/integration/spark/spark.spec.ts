bced24df8126bc1e150793fa89b2aa1c3151c410
William Kelley <williamkelley9@gmail.com>
2021-10-07 18:40:25 -0400

    chore: fix broken e2e test

--- apps/spark-e2e/src/integration/spark/spark.spec.ts
+++ apps/spark-e2e/src/integration/spark/spark.spec.ts
@@ -1,5 +1,5 @@
 describe('spark: Spark component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=prenda-spark-dropdown'));
+  beforeEach(() => cy.visit('/iframe.html?id=pds-ps-button--configurable'));
 
   it('should render the component', () => {
     cy.get('button').should('contain', 'Label');
