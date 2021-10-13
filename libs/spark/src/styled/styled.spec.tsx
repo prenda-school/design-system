621c90e4d7f71370ef84aff7ac84a8f6aa58105a
William Kelley <williamkelley9@gmail.com>
2021-10-13 12:26:25 -0400

    feat(styled): supply custom theme by default

---
+++ libs/spark/src/styled/styled.spec.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { stub } from 'sinon';
+import styled from './styled';
+import initialTheme from '../theme/initialTheme';
+
+describe('styled', () => {
+  const Empty = () => <div />;
+
+  it("should supply Spark's theme by default", () => {
+    const styles = stub().returns({});
+    const StyledComponent = styled(Empty)(styles);
+
+    render(<StyledComponent />);
+
+    expect(styles.args[0][0].theme).toEqual(initialTheme);
+  });
+});
