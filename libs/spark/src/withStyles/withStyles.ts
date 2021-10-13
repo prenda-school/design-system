c954444cf50fe6dc609401d15ee4cc73566ab0a9
William Kelley <williamkelley9@gmail.com>
2021-10-13 12:24:18 -0400

    feat(withStyles): supply custom theme by default

--- libs/spark/src/withStyles/withStyles.ts
+++ libs/spark/src/withStyles/withStyles.ts
@@ -1,2 +1,31 @@
-export { default } from '@material-ui/core/styles/withStyles';
+import { PropInjector } from '@material-ui/types';
+import {
+  default as muiWithStyles,
+  StyledComponentProps,
+  Styles,
+  WithStyles,
+  WithStylesOptions,
+} from '@material-ui/core/styles/withStyles';
+import type { Theme } from '../theme';
+import initialTheme from '../theme/initialTheme';
+
 export * from '@material-ui/core/styles/withStyles';
+
+export default function withStyles<
+  ClassKey extends string,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Options extends WithStylesOptions<Theme> = {},
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Props extends object = {}
+>(
+  styles: Styles<Theme, Props, ClassKey>,
+  options?: Options
+): PropInjector<
+  WithStyles<ClassKey, Options['withTheme']>,
+  StyledComponentProps<ClassKey> & Props
+> {
+  return muiWithStyles(styles, {
+    defaultTheme: initialTheme,
+    ...options,
+  });
+}
