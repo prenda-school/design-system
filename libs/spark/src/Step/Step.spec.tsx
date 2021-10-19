1364e75e951546f62665efbef546ac918e34325e
William Kelley <williamkelley9@gmail.com>
2021-10-19 10:16:55 -0400

    feat(Step): re-export with custom styles; tests; stories

---
+++ libs/spark/src/Step/Step.spec.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import Step from './Step';
+
+/**
+ * A component that can be used as a child of `Step`. It passes through all unrelated props to the underlying `div`. The props passed from `Step` are intercepted
+ */
+function Child({
+  active,
+  alternativeLabel,
+  completed,
+  disabled,
+  expanded,
+  icon,
+  last,
+  orientation,
+  ...other
+}) {
+  return <div {...other}>{icon}</div>;
+}
+
+describe('Step', () => {
+  it('passes `icon` to its child, with value according to given `index`', () => {
+    const { getByText } = render(
+      <Step index={0}>
+        {/* @ts-expect-error Type '{}' is missing the following properties */}
+        <Child />
+      </Step>
+    );
+    expect(getByText('1')).not.toBe(null);
+  });
+});
