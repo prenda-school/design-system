56ae69af0e5599d6e2cb162edeac9e844a32f93f
William Kelley <williamkelley9@gmail.com>
2021-10-19 10:09:53 -0400

    feat(StepConnector): initial implementation; tests; stories

---
+++ libs/spark/src/StepConnector/StepConnector.stories.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { StepConnector, StepConnectorProps } from '..';
+
+export default {
+  title: 'PDS/@ps/StepConnector',
+  component: StepConnector,
+} as Meta;
+
+const Template = (args: StepConnectorProps) => <StepConnector {...args} />;
+
+export const Configurable: Story = Template.bind({});
+
+export const Horizontal: Story = Template.bind({});
+Horizontal.args = { orientation: 'horizontal' };
+Horizontal.storyName = 'horizontal';
+
+export const HorizontalActive: Story = Template.bind({});
+HorizontalActive.args = { active: true };
+HorizontalActive.storyName = 'horizontal active';
+
+export const HorizontalCompleted: Story = Template.bind({});
+HorizontalCompleted.args = { completed: true };
+HorizontalCompleted.storyName = 'horizontal completed';
+
+export const HorizontalDisabled: Story = Template.bind({});
+HorizontalDisabled.args = { disabled: true };
+HorizontalDisabled.storyName = 'horizontal disabled';
+
+export const Vertical: Story = Template.bind({});
+Vertical.args = { orientation: 'vertical' };
+Vertical.storyName = 'vertical';
+
+export const VerticalActive: Story = Template.bind({});
+VerticalActive.args = { orientation: 'vertical', active: true };
+VerticalActive.storyName = 'vertical active';
+
+export const VerticalCompleted: Story = Template.bind({});
+VerticalCompleted.args = { orientation: 'vertical', completed: true };
+VerticalCompleted.storyName = 'vertical completed';
+
+export const VerticalDisabled: Story = Template.bind({});
+VerticalDisabled.args = { orientation: 'vertical', disabled: true };
+VerticalDisabled.storyName = 'vertical disabled';
