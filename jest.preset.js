e22abe2057ee6f13e89d668706dba14dda8077ac
William Kelley <williamkelley9@gmail.com>
2021-10-22 12:11:20 -0400

    chore: jest preset file syntax won't compile

--- jest.preset.js
+++ jest.preset.js
@@ -1,7 +1,8 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-const setupFiles = (nxPreset.setupFiles ?? []).concat([
-  '../../jest.overrides.js',
-]);
+const setupFiles = (nxPreset.setupFiles === undefined
+  ? []
+  : nxPreset.setupFiles
+).concat(['../../jest.overrides.js']);
 
 module.exports = { ...nxPreset, setupFiles };
