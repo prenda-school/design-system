fdffb9586a66df2f65ce0776892be972e3996005
William Kelley <williamkelley9@gmail.com>
2021-11-12 23:43:09 -0500

    fix: NavBarButton losing underlying button styling

--- libs/spark/src/NavBarButton/NavBarButton.tsx
+++ libs/spark/src/NavBarButton/NavBarButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { default as Button, ButtonProps } from '@material-ui/core/Button';
+import { default as Button, ButtonProps } from '../Button';
 import withStyles from '../withStyles';
 
 export type NavBarButtonProps = Omit<ButtonProps, 'variant' | 'color'>;
