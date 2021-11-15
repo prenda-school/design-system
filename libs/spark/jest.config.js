6982962cb5c504c2d3d64f1614e38c06e4cdb996
William Kelley <williamkelley9@gmail.com>
2021-11-15 12:37:45 -0500

    chore(npm): install jest dom util pkg

--- libs/spark/jest.config.js
+++ libs/spark/jest.config.js
@@ -6,4 +6,10 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   coverageDirectory: '../../coverage/libs/spark',
+  /**
+   * Enable `@testing-library/jest-dom` matchers.
+   *
+   * @see https://jestjs.io/docs/configuration#setupfilesafterenv-array
+   */
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
