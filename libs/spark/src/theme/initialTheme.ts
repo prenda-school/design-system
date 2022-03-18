87087208ff51e9e81a331a9cee9e4f51fdf32f5f
William Kelley <williamkelley9@gmail.com>
2022-03-18 14:13:02 -0700

    fix(unstable_theme): make unstable theme properties available on default theme

--- libs/spark/src/theme/initialTheme.ts
+++ libs/spark/src/theme/initialTheme.ts
@@ -2,9 +2,16 @@ import { default as createTheme } from '@material-ui/core/styles/createTheme';
 import palette from './palette';
 import shadows from './shadows';
 import typography from './typography';
+import unstable_palette from './unstable_palette';
+import unstable_typography from './unstable_typography';
 
-export default createTheme({
+const initialTheme = createTheme({
   palette,
   shadows,
   typography,
 });
+
+initialTheme.unstable_palette = unstable_palette;
+initialTheme.unstable_typography = unstable_typography;
+
+export default initialTheme;
