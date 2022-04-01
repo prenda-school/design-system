a0f2cc5f133dde81dc14f7bb5d73404b7ed8bbb4
William Kelley <williamkelley9@gmail.com>
2022-04-01 13:56:12 -0400

    feat(CssBaseline): change default color, line-height (#381)

--- libs/spark/src/CssBaseline/CssBaseline.ts
+++ libs/spark/src/CssBaseline/CssBaseline.ts
@@ -24,8 +24,10 @@ const CssBaseline = withStyles(
       },
       body: {
         backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.dark,
         fontFamily: theme.typography.fontFamily,
         fontSize: theme.typography.fontSize,
+        lineHeight: theme.typography['paragraph-md'].lineHeight,
         margin: 0, // Remove the margin in all browsers.
         '@media print': {
           // Save printer ink.
@@ -36,9 +38,6 @@ const CssBaseline = withStyles(
         '&::backdrop': {
           backgroundColor: theme.palette.background.default,
         },
-        /* :TEMPORARY: customize `lineHeight` and `color` after #380 is merged -- will change all stories with text outside of components */
-        lineHeight: theme.typography.body2.lineHeight,
-        color: theme.palette.text.primary,
       },
     },
   }),
