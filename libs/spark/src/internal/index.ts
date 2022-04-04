87f6f791e3d0d78c810faeedb01d320fba0e67e1
Scott Lingner <56003456+slingner@users.noreply.github.com>
2022-04-04 09:14:58 -0700

    add unstable svg icons for section message severity levels

--- libs/spark/src/internal/index.ts
+++ libs/spark/src/internal/index.ts
@@ -10,4 +10,9 @@ export { default as Cross } from './Cross';
 export { default as CrossSmall } from './CrossSmall';
 export { default as InfoFilled } from './InfoFilled';
 
+export { default as Unstable_AlertOctagon } from './Unstable_AlertOctagon';
+export { default as Unstable_AlertTriangle } from './Unstable_AlertTriangle';
+export { default as Unstable_CheckCircle2 } from './Unstable_CheckCircle2';
 export { default as Unstable_ChevronDown } from './Unstable_ChevronDown';
+export { default as Unstable_Cross } from './Unstable_Cross';
+export { default as Unstable_Info } from './Unstable_Info';
