82256990425443dc34501b97cddc11db41827634
Scott Lingner <56003456+slingner@users.noreply.github.com>
2022-04-05 11:18:59 -0700

    rename test file, embed SectionMessageTitle inside SectionMessage

--- libs/spark/src/Unstable_SectionMessage/Unstable_SectionMessage.tsx
+++ libs/spark/src/Unstable_SectionMessage/Unstable_SectionMessage.tsx
@@ -10,7 +10,40 @@ import {
 } from '../internal';
 import makeStyles from '../makeStyles';
 import { StandardProps } from '../utils';
-import Unstable_SectionMessageTitle from '../Unstable_SectionMessageTitle';
+
+interface Unstable_SectionMessageTitleProps
+  extends StandardProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    Unstable_SectionMessageTitleClassKey
+  > {
+  /**
+   * The content of the component.
+   */
+  children?: string;
+}
+
+type Unstable_SectionMessageTitleClassKey = 'root';
+
+const useStylesTitle = makeStyles<Unstable_SectionMessageTitleClassKey>(
+  (theme) => ({
+    root: {
+      ...theme.unstable_typography.label,
+      color: theme.unstable_palette.text.heading,
+      marginBottom: 4,
+    },
+  }),
+  { name: 'MuiSparkUnstable_SectionMessageTitle' }
+);
+
+const Unstable_SectionMessageTitle = function Unstable_SectionMessageTitle(
+  props: Unstable_SectionMessageTitleProps
+) {
+  const { children, className } = props;
+
+  const classes = useStylesTitle();
+
+  return <div className={clsx(classes.root, className)}>{children}</div>;
+};
 
 export type Color = 'success' | 'info' | 'warning' | 'error';
 
@@ -155,7 +188,9 @@ const Unstable_SectionMessage = React.forwardRef(
         ) : null}
 
         <div className={clsx(classesProp?.message, classes.message)}>
-          {title ? <Unstable_SectionMessageTitle title={title} /> : null}
+          {title ? (
+            <Unstable_SectionMessageTitle>{title}</Unstable_SectionMessageTitle>
+          ) : null}
           {children}
         </div>
         {action != null ? (
@@ -165,15 +200,7 @@ const Unstable_SectionMessage = React.forwardRef(
         ) : null}
         {action == null && onClose ? (
           <div className={clsx(classesProp?.action, classes.action)}>
-            {/* <IconButton
-            size="small"
-            aria-label={closeText}
-            title={closeText}
-            color="inherit"
-            onClick={onClose}
-          > */}
             <Unstable_Cross fontSize="medium" color="normal" />
-            {/* </IconButton> */}
           </div>
         ) : null}
       </Paper>
