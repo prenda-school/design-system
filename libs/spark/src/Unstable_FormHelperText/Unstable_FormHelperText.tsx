c5352b2a18f8bc9039050c5d24c180f183459dff
William Kelley <williamkelley9@gmail.com>
2022-04-05 17:14:24 -0400

    feat(Unstable_FormHelperText): initial implementation (#387)

---
+++ libs/spark/src/Unstable_FormHelperText/Unstable_FormHelperText.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import MuiFormHelperText, {
+  FormHelperTextProps as MuiFormHelperTextProps,
+} from '@material-ui/core/FormHelperText';
+import clsx from 'clsx';
+import makeStyles from '../makeStyles';
+import { OverridableComponent, OverrideProps } from '../utils';
+
+export interface Unstable_FormHelperTextTypeMap<
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  P = {},
+  D extends React.ElementType = 'p'
+> {
+  props: P & Omit<MuiFormHelperTextProps, 'filled' | 'margin' | 'variant'>;
+  defaultComponent: D;
+  classKey: Unstable_FormHelperTextClassKey;
+}
+
+export type Unstable_FormHelperTextClassKey = 'root';
+
+export type Unstable_FormHelperTextProps<
+  D extends React.ElementType = Unstable_FormHelperTextTypeMap['defaultComponent'],
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  P = {}
+> = OverrideProps<Unstable_FormHelperTextTypeMap<P, D>, D>;
+
+const useStyles = makeStyles<Unstable_FormHelperTextClassKey>(
+  (theme) => ({
+    root: (props: Unstable_FormHelperTextProps) => ({
+      ...theme.unstable_typography.description,
+      alignItems: 'center',
+      color: theme.unstable_palette.text.subdued,
+      display: 'flex',
+      gap: 4,
+      letterSpacing: 0,
+      marginLeft: 4, // match margin of Input
+      marginTop: 4,
+      /* Styles applied to the `input` element if `disabled={true}`. */
+      ...(props.disabled && {
+        color: theme.unstable_palette.text.disabled,
+      }),
+      // duped because underlying component can set this independently based on form control context
+      '&.Mui-disabled': {
+        color: theme.unstable_palette.text.disabled,
+      },
+      /* Styles applied to the `input` element if `disabled={true}`. */
+      ...(props.error && {
+        color: theme.unstable_palette.red[700],
+      }),
+      // duped because underlying component can set this independently based on form control context
+      '&.Mui-error': {
+        color: theme.unstable_palette.red[700],
+      },
+    }),
+  }),
+  { name: 'MuiSparkUnstable_FormHelperText' }
+);
+
+const Unstable_FormHelperText: OverridableComponent<Unstable_FormHelperTextTypeMap> = React.forwardRef(
+  function Unstable_FormHelperText(props, ref) {
+    const { classes: classesProp, disabled, error, ...other } = props;
+
+    const classes = useStyles({ disabled, error });
+
+    return (
+      <MuiFormHelperText
+        classes={{ root: clsx(classes.root, classesProp?.root) }}
+        disabled={disabled}
+        error={error}
+        ref={ref}
+        {...other}
+      />
+    );
+  }
+);
+
+export default Unstable_FormHelperText;
--- libs/spark/src/Unstable_InputAdornment/Unstable_InputAdornment.stories.tsx
+++ libs/spark/src/Unstable_InputAdornment/Unstable_InputAdornment.stories.tsx
@@ -9,7 +9,7 @@ import { sparkThemeProvider } from '../../stories';
 
 const Search = unstable_createSvgIcon(
   <path d="M2.75 11a8.25 8.25 0 1116.5 0 8.25 8.25 0 01-16.5 0zM11 1.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75a9.712 9.712 0 006.344-2.346l3.126 3.126a.75.75 0 101.06-1.06l-3.126-3.126A9.712 9.712 0 0020.75 11c0-5.385-4.365-9.75-9.75-9.75z" />,
-  'SbSearch'
+  'Sb_Search'
 );
 
 // underlying props don't have descriptions
