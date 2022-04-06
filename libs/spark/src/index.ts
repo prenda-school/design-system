a7a1e9fded1853f0f0abbd1885228fd9d9fab97a
Scott Lingner <56003456+slingner@users.noreply.github.com>
2022-04-06 12:32:10 -0700

    feat(Unstable_SectionMessage): initial implementation (#383)
    Co-authored-by: William Kelley <williamkelley9@gmail.com>

--- libs/spark/src/index.ts
+++ libs/spark/src/index.ts
@@ -233,6 +233,9 @@ export * from './Unstable_InputLabel';
 export { default as Unstable_Link } from './Unstable_Link';
 export * from './Unstable_Link';
 
+export { default as Unstable_SectionMessage } from './Unstable_SectionMessage';
+export * from './Unstable_SectionMessage';
+
 export { default as Unstable_Select } from './Unstable_Select';
 export * from './Unstable_Select';
 
---
+++ libs/spark/src/internal/Unstable_Info.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import unstable_createSvgIcon from '../unstable_createSvgIcon';
+
+export default unstable_createSvgIcon(
+  <path d="M12 2.75a9.25 9.25 0 100 18.5 9.25 9.25 0 000-18.5zM1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12 17.937 22.75 12 22.75 1.25 17.937 1.25 12zM12 10.25a.75.75 0 01.75.75v5a.75.75 0 01-1.5 0v-5a.75.75 0 01.75-.75zM12 9a1 1 0 100-2 1 1 0 000 2z" />,
+  'Unstable_Info'
+);
