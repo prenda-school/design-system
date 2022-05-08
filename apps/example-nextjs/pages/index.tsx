2a850b3cb35e0115ae14b74d13b975956d86948d
William Kelley <williamkelley9@gmail.com>
2022-05-07 22:08:31 -0400

    feat(example-nextjs): setup for Mui v4 SSR

--- apps/example-nextjs/pages/index.tsx
+++ apps/example-nextjs/pages/index.tsx
@@ -1,9 +1,20 @@
 import styled from '@emotion/styled';
+import { Box, Typography } from '@material-ui/core';
 
-const Container = styled.div``;
+const Container = styled.div({
+  color: '#091e42',
+});
 
 export function Index() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Box my={4}>
+        <Typography component="h1" color="inherit" gutterBottom>
+          Example with Next.js, TypeScript, Emotion
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
 
 export default Index;
