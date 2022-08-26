4e891b904078934b72ad88f1d597825902cae32c
William Kelley <williamkelley9@gmail.com>
2022-08-26 12:52:39 -0400

    chore: fix e2e test visiting wrong page

--- apps/spark-e2e/src/integration/spark/spark.spec.ts
+++ apps/spark-e2e/src/integration/spark/spark.spec.ts
@@ -1,5 +1,5 @@
 describe('spark: Spark component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=ps-button--configurable'));
+  beforeEach(() => cy.visit('/iframe.html?id=ps-button--default'));
 
   it('should render the component', () => {
     cy.get('button').should('contain', 'Label');
