3f0fc2299558ab2977c4a254b07d0c766057e208
William Kelley <williamkelley9@gmail.com>
2022-11-01 10:57:15 -0400

    feat(theme): initial implementation of unstable_elevations

--- libs/spark/src/theme/theme.ts
+++ libs/spark/src/theme/theme.ts
@@ -3,12 +3,16 @@ import initialTheme from './initialTheme';
 import overrides from './overrides';
 import props from './props';
 import type {} from './themeAugmentation';
+import unstable_elevations, {
+  Unstable_Elevations,
+} from './unstable_elevations';
 import unstable_palette, { Unstable_Palette } from './unstable_palette';
 import unstable_typography, {
   Unstable_TypographyOptions,
 } from './unstable_typography';
 
 export interface Theme extends MuiTheme {
+  unstable_elevations: Unstable_Elevations;
   unstable_palette: Unstable_Palette;
   unstable_typography: Unstable_TypographyOptions;
 }
@@ -22,6 +26,7 @@ const theme = createTheme({
   overrides: overrides(initialTheme),
 });
 
+theme.unstable_elevations = unstable_elevations;
 theme.unstable_palette = unstable_palette;
 theme.unstable_typography = unstable_typography;
 
---
+++ libs/spark/src/theme/unstable_elevations.stories.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import type { Meta, Story } from '@storybook/react/types-6-0';
+import { styled, theme } from '..';
+
+export default {
+  title: '@ps/theme/elevations',
+} as Meta;
+
+const _Elevations = styled(function Elevations(props) {
+  return (
+    <div {...props}>
+      <h1 className="title">Elevation</h1>
+      <span className="code">theme.unstable_elevations</span>
+      <div className="content">
+        <div>
+          <div
+            className="square"
+            style={{ boxShadow: theme.unstable_elevations[0] }}
+          />
+          <span className="name">E0</span>
+          <span className="code">elevations[0]</span>
+        </div>
+        <div>
+          <div
+            className="square"
+            style={{ boxShadow: theme.unstable_elevations[100] }}
+          />
+          <span className="name">E100</span>
+          <span className="code">elevations[100]</span>
+        </div>
+        <div>
+          <div
+            className="square"
+            style={{ boxShadow: theme.unstable_elevations[200] }}
+          />
+          <span className="name">E200</span>
+          <span className="code">elevations[200]</span>
+        </div>
+        <div>
+          <div
+            className="square"
+            style={{ boxShadow: theme.unstable_elevations[300] }}
+          />
+          <span className="name">E300</span>
+          <span className="code">elevations[300]</span>
+        </div>
+        <div>
+          <div
+            className="square"
+            style={{ boxShadow: theme.unstable_elevations[400] }}
+          />
+          <span className="name">E400</span>
+          <span className="code">elevations[400]</span>
+        </div>
+        <div>
+          <div
+            className="square"
+            style={{ boxShadow: theme.unstable_elevations[500] }}
+          />
+          <span className="name">E500</span>
+          <span className="code">elevations[500]</span>
+        </div>
+      </div>
+    </div>
+  );
+})(({ theme }) => ({
+  paddingLeft: 8,
+  '& .title': {
+    ...theme.unstable_typography.T22,
+  },
+  '& .content': {
+    display: 'flex',
+    gap: 40,
+  },
+  '& .square': {
+    backgroundColor: theme.unstable_palette.neutral[0],
+    borderRadius: 8,
+    height: 80,
+    width: 80,
+  },
+  '& .name': {
+    ...theme.unstable_typography.T14,
+    display: 'block',
+    marginTop: 16,
+  },
+  '& .code': {
+    ...theme.unstable_typography.code,
+  },
+}));
+
+export const Elevations: Story = () => <_Elevations />;
