f064480d13871e105bfa7d4b428a2e2cd5455b9e
William Kelley <williamkelley9@gmail.com>
2022-11-03 12:35:55 -0400

    feat(SectionMessage): props-static styles

--- libs/spark/src/Unstable_SectionMessage/Unstable_SectionMessage.tsx
+++ libs/spark/src/Unstable_SectionMessage/Unstable_SectionMessage.tsx
@@ -6,6 +6,7 @@ import Unstable_Alert, {
   Unstable_AlertProps,
 } from '../internal/Unstable_Alert';
 import { StandardProps } from '../utils';
+import withStyles from '../withStyles';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Unstable_SectionMessageProps
@@ -18,9 +19,80 @@ export interface Unstable_SectionMessageProps
 
 export type Unstable_SectionMessageClassKey = Unstable_AlertClassKey | 'title';
 
-const useStyles = makeStyles<Unstable_SectionMessageClassKey>(
+type PrivateClassKey =
+  | 'root-severity-error'
+  | 'root-severity-info'
+  | 'root-severity-success'
+  | 'root-severity-warning'
+  | 'icon-severity-error'
+  | 'icon-severity-info'
+  | 'icon-severity-success'
+  | 'icon-severity-warning';
+
+const usePrivateStyles = makeStyles<PrivateClassKey>(
   (theme) => ({
-    root: (props: Unstable_SectionMessageProps) => ({
+    'root-severity-error': {
+      backgroundColor: theme.unstable_palette.red[100],
+      borderColor: theme.unstable_palette.red[700],
+    },
+    'root-severity-info': {
+      backgroundColor: theme.unstable_palette.blue[100],
+      borderColor: theme.unstable_palette.blue[600],
+    },
+    'root-severity-success': {
+      backgroundColor: theme.unstable_palette.green[100],
+      borderColor: theme.unstable_palette.green[600],
+    },
+    'root-severity-warning': {
+      backgroundColor: theme.unstable_palette.yellow[100],
+      borderColor: theme.unstable_palette.yellow[600],
+    },
+    'icon-severity-error': {
+      color: theme.unstable_palette.red[700],
+    },
+    'icon-severity-info': {
+      color: theme.unstable_palette.blue[600],
+    },
+    'icon-severity-success': {
+      color: theme.unstable_palette.green[600],
+    },
+    'icon-severity-warning': {
+      color: theme.unstable_palette.yellow[600],
+    },
+  }),
+  { name: 'MuiSparkPrivate-SectionMessage' }
+);
+
+const Unstable_SectionMessage = forwardRef<
+  unknown,
+  Unstable_SectionMessageProps
+>(function Unstable_SectionMessage(props, ref) {
+  const { children, classes, severity = 'info', title, ...other } = props;
+
+  const privateClasses = usePrivateStyles({ severity });
+
+  return (
+    <Unstable_Alert
+      classes={{
+        root: clsx(classes.root, privateClasses[`root-severity-${severity}`]),
+        icon: clsx(classes.icon, privateClasses[`icon-severity-${severity}`]),
+        message: classes.message,
+        action: classes.action,
+      }}
+      severity={severity}
+      ref={ref}
+      {...other}
+    >
+      {title ? <div className={classes.title}>{title}</div> : null}
+
+      {children}
+    </Unstable_Alert>
+  );
+});
+
+export default withStyles<Unstable_SectionMessageClassKey>(
+  (theme) => ({
+    root: {
       alignItems: 'flex-start',
       borderRadius: 4,
       display: 'flex',
@@ -28,43 +100,13 @@ const useStyles = makeStyles<Unstable_SectionMessageClassKey>(
       padding: 24,
       borderWidth: 1,
       borderStyle: 'solid',
-      /* severity */
-      ...(props.severity === 'error' && {
-        backgroundColor: theme.unstable_palette.red[100],
-        borderColor: theme.unstable_palette.red[700],
-      }),
-      ...(props.severity === 'info' && {
-        backgroundColor: theme.unstable_palette.blue[100],
-        borderColor: theme.unstable_palette.blue[600],
-      }),
-      ...(props.severity === 'success' && {
-        backgroundColor: theme.unstable_palette.green[100],
-        borderColor: theme.unstable_palette.green[600],
-      }),
-      ...(props.severity === 'warning' && {
-        backgroundColor: theme.unstable_palette.yellow[100],
-        borderColor: theme.unstable_palette.yellow[600],
-      }),
-    }),
-    icon: (props: Unstable_SectionMessageProps) => ({
+    },
+    icon: {
       color: theme.unstable_palette.text.icon,
       display: 'flex',
       fontSize: theme.unstable_typography.pxToRem(24),
       lineHeight: 1,
-      /* severity */
-      ...(props.severity === 'error' && {
-        color: theme.unstable_palette.red[700],
-      }),
-      ...(props.severity === 'info' && {
-        color: theme.unstable_palette.blue[600],
-      }),
-      ...(props.severity === 'success' && {
-        color: theme.unstable_palette.green[600],
-      }),
-      ...(props.severity === 'warning' && {
-        color: theme.unstable_palette.yellow[600],
-      }),
-    }),
+    },
     message: {
       ...theme.unstable_typography.body,
       color: theme.unstable_palette.text.body,
@@ -81,41 +123,4 @@ const useStyles = makeStyles<Unstable_SectionMessageClassKey>(
     },
   }),
   { name: 'MuiSparkUnstable_SectionMessage' }
-);
-
-const Unstable_SectionMessage = forwardRef<
-  unknown,
-  Unstable_SectionMessageProps
->(function Unstable_SectionMessage(props, ref) {
-  const {
-    children,
-    classes: classesProp,
-    severity = 'info',
-    title,
-    ...other
-  } = props;
-
-  const classes = useStyles({ severity });
-
-  return (
-    <Unstable_Alert
-      classes={{
-        root: clsx(classes.root, classesProp?.root),
-        icon: clsx(classes.icon, classesProp?.icon),
-        message: clsx(classes.message, classesProp?.message),
-        action: clsx(classes.action, classesProp?.action),
-      }}
-      severity={severity}
-      ref={ref}
-      {...other}
-    >
-      {title ? (
-        <div className={clsx(classes.title, classesProp?.title)}>{title}</div>
-      ) : null}
-
-      {children}
-    </Unstable_Alert>
-  );
-});
-
-export default Unstable_SectionMessage;
+)(Unstable_SectionMessage) as typeof Unstable_SectionMessage;
