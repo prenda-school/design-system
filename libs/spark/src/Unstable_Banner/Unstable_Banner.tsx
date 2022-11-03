59192e16708ce475214544a9c9b41ceb07fb3293
William Kelley <williamkelley9@gmail.com>
2022-11-03 14:26:14 -0400

    feat(Banner): props-static styles (#584)

--- libs/spark/src/Unstable_Banner/Unstable_Banner.tsx
+++ libs/spark/src/Unstable_Banner/Unstable_Banner.tsx
@@ -5,73 +5,79 @@ import Unstable_Alert, {
   Unstable_AlertClassKey,
   Unstable_AlertProps,
 } from '../internal/Unstable_Alert';
+import withStyles from '../withStyles';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Unstable_BannerProps extends Unstable_AlertProps {}
 
 export type Unstable_BannerClassKey = Unstable_AlertClassKey;
 
-const useStyles = makeStyles<Unstable_BannerClassKey>(
+type PrivateClassKey =
+  | 'root-severity-error'
+  | 'root-severity-info'
+  | 'root-severity-success'
+  | 'root-severity-warning'
+  | 'icon-severity-error'
+  | 'icon-severity-info'
+  | 'icon-severity-success'
+  | 'icon-severity-warning'
+  | 'message-severity-error'
+  | 'message-severity-info'
+  | 'message-severity-success'
+  | 'message-severity-warning';
+
+const usePrivateStyles = makeStyles<PrivateClassKey>(
   (theme) => ({
-    root: (props: Unstable_BannerProps) => ({
-      alignItems: 'flex-start',
-      display: 'flex',
-      gap: 16,
-      padding: 24,
-      /* severity */
-      ...(props.severity === 'error' && {
-        backgroundColor: theme.unstable_palette.red[700],
-      }),
-      ...(props.severity === 'info' && {
-        backgroundColor: theme.unstable_palette.blue[700],
-      }),
-      ...(props.severity === 'success' && {
-        backgroundColor: theme.unstable_palette.green[700],
-      }),
-      ...(props.severity === 'warning' && {
-        backgroundColor: theme.unstable_palette.yellow[600],
-      }),
-    }),
-    icon: (props: Unstable_BannerProps) => ({
+    'root-severity-error': {
+      backgroundColor: theme.unstable_palette.red[700],
+    },
+    'root-severity-info': {
+      backgroundColor: theme.unstable_palette.blue[700],
+    },
+    'root-severity-success': {
+      backgroundColor: theme.unstable_palette.green[700],
+    },
+    'root-severity-warning': {
+      backgroundColor: theme.unstable_palette.yellow[600],
+    },
+    'icon-severity-error': {
       color: theme.unstable_palette.neutral[0],
-      display: 'flex',
-      fontSize: theme.unstable_typography.pxToRem(24),
-      lineHeight: 1,
-      paddingBottom: 4,
-      paddingTop: 4,
-      /* severity */
-      ...(props.severity === 'warning' && {
-        color: theme.unstable_palette.neutral[600],
-      }),
-    }),
-    message: (props: Unstable_BannerProps) => ({
-      ...theme.unstable_typography.body,
+    },
+    'icon-severity-info': {
       color: theme.unstable_palette.neutral[0],
-      flexGrow: 2,
-      paddingTop: 4,
-      /* severity */
-      ...(props.severity === 'warning' && {
-        color: theme.unstable_palette.neutral[600],
-      }),
-    }),
-    action: {
-      justifySelf: 'flex-end',
-      marginTop: -2,
+    },
+    'icon-severity-success': {
+      color: theme.unstable_palette.neutral[0],
+    },
+    'icon-severity-warning': {
+      color: theme.unstable_palette.neutral[600],
+    },
+    'message-severity-error': {
+      color: theme.unstable_palette.neutral[0],
+    },
+    'message-severity-info': {
+      color: theme.unstable_palette.neutral[0],
+    },
+    'message-severity-success': {
+      color: theme.unstable_palette.neutral[0],
+    },
+    'message-severity-warning': {
+      color: theme.unstable_palette.neutral[600],
     },
   }),
-  { name: 'MuiSparkUnstable_Banner' }
+  { name: 'MuiSparkPrivate-Banner' }
 );
 
 const Unstable_Banner = forwardRef<unknown, Unstable_BannerProps>(
   function Unstable_Banner(props, ref) {
     const {
-      classes: classesProp,
+      classes,
       severity = 'info',
       CloseProps: ClosePropsProp,
       ...other
     } = props;
 
-    const classes = useStyles({ severity });
+    const privateClasses = usePrivateStyles();
 
     let CloseProps: Unstable_AlertProps['CloseProps'] = ClosePropsProp;
     if (['info', 'success', 'error'].includes(severity)) {
@@ -81,10 +87,13 @@ const Unstable_Banner = forwardRef<unknown, Unstable_BannerProps>(
     return (
       <Unstable_Alert
         classes={{
-          root: clsx(classes.root, classesProp?.root),
-          icon: clsx(classes.icon, classesProp?.icon),
-          message: clsx(classes.message, classesProp?.message),
-          action: clsx(classes.action, classesProp?.action),
+          root: clsx(classes.root, privateClasses[`root-severity-${severity}`]),
+          icon: clsx(classes.icon, privateClasses[`icon-severity-${severity}`]),
+          message: clsx(
+            classes.message,
+            privateClasses[`message-severity-${severity}`]
+          ),
+          action: classes.action,
         }}
         severity={severity}
         ref={ref}
@@ -95,4 +104,30 @@ const Unstable_Banner = forwardRef<unknown, Unstable_BannerProps>(
   }
 );
 
-export default Unstable_Banner;
+export default withStyles<Unstable_BannerClassKey>(
+  (theme) => ({
+    root: {
+      alignItems: 'flex-start',
+      display: 'flex',
+      gap: 16,
+      padding: 24,
+    },
+    icon: {
+      display: 'flex',
+      fontSize: theme.unstable_typography.pxToRem(24),
+      lineHeight: 1,
+      paddingBottom: 4,
+      paddingTop: 4,
+    },
+    message: {
+      ...theme.unstable_typography.body,
+      flexGrow: 2,
+      paddingTop: 4,
+    },
+    action: {
+      justifySelf: 'flex-end',
+      marginTop: -2,
+    },
+  }),
+  { name: 'MuiSparkUnstable_Banner' }
+)(Unstable_Banner) as typeof Unstable_Banner;
