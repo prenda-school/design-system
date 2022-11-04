679ee912726a1719a4be1b8a2e37f6dd7a55a0a1
William Kelley <williamkelley9@gmail.com>
2022-11-03 22:19:24 -0400

    refactor(FormGroup): adopt withClasses pattern

--- libs/spark/src/Unstable_FormGroup/Unstable_FormGroup.tsx
+++ libs/spark/src/Unstable_FormGroup/Unstable_FormGroup.tsx
@@ -2,9 +2,7 @@ import React, { forwardRef } from 'react';
 import MuiFormGroup, {
   FormGroupProps as MuiFormGroupProps,
 } from '@material-ui/core/FormGroup';
-import clsx from 'clsx';
-import makeStyles from '../makeStyles';
-import { StyledComponentProps } from '../withStyles';
+import withStyles, { StyledComponentProps } from '../withStyles';
 
 export interface Unstable_FormGroupProps
   extends Omit<MuiFormGroupProps, 'classes'>,
@@ -12,7 +10,7 @@ export interface Unstable_FormGroupProps
 
 export type Unstable_FormGroupClassKey = 'root';
 
-const useStyles = makeStyles<Unstable_FormGroupClassKey>(
+const withClasses = withStyles<Unstable_FormGroupClassKey>(
   {
     /** Styles applied to the root element. */
     root: {
@@ -26,18 +24,8 @@ const useStyles = makeStyles<Unstable_FormGroupClassKey>(
 
 const Unstable_FormGroup = forwardRef<unknown, Unstable_FormGroupProps>(
   function Unstable_FormGroup(props, ref) {
-    const { classes: classesProp, ...other } = props;
-
-    const classes = useStyles();
-
-    return (
-      <MuiFormGroup
-        classes={{ root: clsx(classes.root, classesProp?.root) }}
-        ref={ref}
-        {...other}
-      />
-    );
+    return <MuiFormGroup ref={ref} {...props} />;
   }
 );
 
-export default Unstable_FormGroup;
+export default withClasses(Unstable_FormGroup) as typeof Unstable_FormGroup;
