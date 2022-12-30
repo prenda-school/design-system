ac6ee6b0ace3a68e0010d2fc25a00a535b25eefb
William Kelley <williamkelley9@gmail.com>
2022-12-30 14:03:05 -0700

    Revert "feat: add example app with nextjs (#401)"
    This reverts commit fc13aa0261678cbdf942ee8a13271cda67623fd4.

--- apps/example-nextjs/lib/makeStyles.ts
+++
@@ -1,6 +0,0 @@
-import { useTheme } from '@material-ui/core/styles';
-import { createMakeAndWithStyles } from 'tss-react';
-
-export const { makeStyles, useStyles, withStyles } = createMakeAndWithStyles({
-  useTheme,
-});
