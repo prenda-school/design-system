7dd9525033122fc749bea7a7fdc54c2ae8b880e5
William Kelley <williamkelley9@gmail.com>
2023-11-16 11:18:37 -0500

    chore: remove all dependencies, including Nx

--- jest.preset.js
+++
@@ -1,7 +0,0 @@
-const nxPreset = require('@nrwl/jest/preset');
-
-const setupFiles = (
-  nxPreset.setupFiles === undefined ? [] : nxPreset.setupFiles
-).concat(['../../jest.overrides.js']);
-
-module.exports = { ...nxPreset, setupFiles };
