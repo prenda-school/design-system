a740d4bfc3a7af2bd7359512d84f22f9ea17d66f
William Kelley <williamkelley9@gmail.com>
2023-11-20 15:52:53 -0500

    chore(nx): generate a storybook host library
    https://nx.dev/recipes/storybook/one-storybook-with-composition#generate-a-storybook-host-library
    
    > npx nx g @nx/react:lib storybook-host \
    --directory=libs/storybook-host \
    --bundler=none \
    --unitTestRunner=none \
    --projectNameAndRootFormat=as-provided \
    --style=none

---
+++ libs/storybook-host/src/lib/storybook-host.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface StorybookHostProps {}
+
+export function StorybookHost(props: StorybookHostProps) {
+  return (
+    <div>
+      <h1>Welcome to StorybookHost!</h1>
+    </div>
+  );
+}
+
+export default StorybookHost;
