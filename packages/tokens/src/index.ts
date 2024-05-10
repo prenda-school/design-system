f80afb275669bccaf7c9917e0a2b5de2c4cce2ed
William Kelley <williamkelley9@gmail.com>
2024-05-10 13:58:52 -0400

    feat(tokens): add skin tone tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,2 +1,9 @@
 export { color } from './color';
 export type { ColorToken, ColorPalette, ColorNestedPalettes } from './color';
+
+export { skinTone } from './skin-tone';
+export type {
+  SkinToneToken,
+  SkinTonePalette,
+  SkinToneNestedPalettes,
+} from './skin-tone';
